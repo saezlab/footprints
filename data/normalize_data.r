cb72f059c35fc2575041f99466ba4960e4f47d5f
mschubert <mschu.dev@gmail.com>
2015-05-08 10:00:27 +0100

    map genes when normalizing

--- data/normalize_data.r
+++ data/normalize_data.r
@@ -2,6 +2,7 @@
 b = import('base')
 io = import('io')
 ma = import('process/microarray')
+idmap = import('process/idmap')
 
 ACCESSION = commandArgs(TRUE)[1] %or% 'E-GEOD-8346'
 OUTFILE = commandArgs(TRUE)[2] %or% paste0(ACCESSION, ".RData")
@@ -9,7 +10,8 @@ OUTFILE = commandArgs(TRUE)[2] %or% paste0(ACCESSION, ".RData")
 # read raw data, normalize, qc
 expr = ArrayExpress::ArrayExpress(ACCESSION) %>%
     ma$qc() %>%
-    ma$normalize()
+    ma$normalize() %>%
+    ma$annotate(summarize="hgnc_symbol")
 
 # save result
 save(expr, file=OUTFILE)
