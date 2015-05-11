3689650f027a41f98b2c348eba8c7751a0db6297
mschubert <mschu.dev@gmail.com>
2015-05-11 16:35:59 +0100

    working gdsc scores

--- model/model2scores.r
+++ model/model2scores.r
@@ -3,11 +3,21 @@
 #   - input data
 #   - gdsc data
 #   - tcga data (later)
+b = import('base')
+io = import('io')
+ar = import('array')
+gdsc = import('data/gdsc')
+
+INFILE = commandArgs(TRUE)[1] %or% "model_linear.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "scores_linear.RData"
+
+# load zscores
+zfit = io$load(INFILE)
 
 # load sanger data
 Ys = gdsc$getDrugResponse('IC50s') # or AUC
-tissues = sg$getTissues(minN=5)
-expr = t(sg$getBasalExpression())
+tissues = gdsc$getTissues(minN=5)
+expr = t(gdsc$getBasalExpression())
 ar$intersect(expr, tissues, Ys, along=1)
 
 # calculate scores on panel
@@ -15,3 +25,5 @@ expr = expr[,intersect(rownames(zfit),colnames(expr))]
 zfit = zfit[intersect(colnames(expr), rownames(zfit)),]
 scores = expr %*% zfit %>%
     ar$map(along=1, base::scale) # pathway across experiments
+
+save(scores, file=OUTFILE)
