78bf869f10af5e2275ea9f2a0267447f3145f7f0
mschubert <mschu.dev@gmail.com>
2015-05-11 16:26:28 +0100

    add working linear model

--- model/model_linear.r
+++ model/model_linear.r
@@ -1,18 +1,35 @@
 # point of this file:
 # - use the zscores to create a linear model
-
+library(dplyr)
 io = import('io')
 ar = import('array')
 st = import('stats')
-gdsc = import('data/gdsc')
-plt = import('plot')
 
 # load speed data, index
-expr = io$load('../data/zscores.RData')
-zscores = expr$zscores
-index = expr$index
-#TODO: inhibiting zscores = -1*zscores (or leave them out for now)
+zobj = io$load('../data/zscores.RData')
+zscores = zobj$zscores
+index = zobj$index
+
+# adjust object for linear modelling
+inh = index$effect=="inhibiting"
+zscores[,inh] = -zscores[,inh]
+zscores = na.omit(zscores) #TODO: handle this better
+zscores = t(zscores)
 
 # fit model to pathway perturbations
-mod = st$lm(zscores~0+pathway, data=index)
-zfit = lm$selectFeatures(zfit, min=pval, n=100)
+mod = st$lm(zscores ~ 0 + pathway, data=index) %>%
+    transmute(gene = response,
+              pathway = sub("^pathway", "", term),
+              zscore = estimate,
+              p.value = p.value) %>%
+    group_by(gene) %>%
+    mutate(p.adj = p.adjust(p.value, method="fdr")) %>%
+    ungroup()
+
+zfit = ar$construct(zscore ~ gene + pathway, data=mod)
+pval = ar$construct(p.adj ~ gene + pathway, data=mod)
+
+#zfit = lm$selectFeatures(zfit, min=pval, n=100)
+zfit[pval>0.01] = 0
+
+save(zfit, file="model_linear.RData")
