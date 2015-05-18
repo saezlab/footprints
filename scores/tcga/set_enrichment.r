e07a309e5444cb9541effbc70571d59e9e660e8d
mschubert <mschu.dev@gmail.com>
2015-05-18 16:38:40 +0100

    tcga set enrichment

---
+++ scores/tcga/set_enrichment.r
@@ -0,0 +1,16 @@
+b = import('base')
+io = import('io')
+gdsc = import('data/gdsc')
+gsea = import('../../genesets/gsea')
+
+INFILE = commandArgs(TRUE)[1] %or% "../../genesets/go.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "go.RData"
+
+# load gene list and expression
+genelist = io$load(INFILE)
+expr = icgc$rna_seq(specimen = icgc$available(rna_seq=TRUE), voom=TRUE)
+
+# perform GSEA
+result = gsea$runGSEA(expr, genelist, transform.normal=TRUE)
+
+save(result, file=OUTFILE)
