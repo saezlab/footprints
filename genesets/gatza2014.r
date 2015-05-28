ab8490f3348c8fb8087617075461a30a56114115
mschubert <mschu.dev@gmail.com>
2015-05-28 10:36:04 +0100

    filter gatza, reactome lists

--- genesets/gatza2014.r
+++ genesets/gatza2014.r
@@ -1,6 +1,30 @@
 b = import('base')
 ar = import('array')
 
+# could include AKT here, but which pathway does it fit?
+# or I need a SPEED-Akt signature to compare
+gatza2014 = list(
+#      "IL-1" = "",
+      "JAK-STAT" = c("STAT1", "STAT3", "IFNA", "IFNG"),
+      "MAPK" = c("PROLIFERATION", "PROLIFERATION (PAM50)"), # AKT, MYC don't really fit
+      "EGFR" = "EGFR",
+      "PI3K" = c("PI3K", "PIK3CA"),
+      "TGFb" = "TGFB",
+      "TNFa" = "TNFA",
+      "VEGF" = "VEGF/HYPOXIA",
+      "Wnt" = "BETA CATENIN",
+#      "Insulin" = "",
+      "Hypoxia" = "HYPOXIA",
+#      "RAR" = "",
+      "p53" = c("P53", "P53 MUT/P53 WT CORR"),
+      "Estrogen" = c("ER", "E2 ACTIVATED (IE) / E2 REPRESSED CORR")#,
+#      "Trail" = "",
+#      "notch" = "",
+#      "NFkB" = "",
+#      "PPAR" = "",
+#      "SHH" = ""
+)
+
 INFILE = commandArgs(TRUE)[1] %or% 'ng.3073-S2.csv'
 
 df = read.csv('ng.3073-S2.csv', row.names=NULL, check.names=FALSE)
--- genesets/reactome.r
+++ genesets/reactome.r
@@ -3,28 +3,37 @@ ar = import('array')
 er = import('./enrichr')
 
 reactome = list(#"H2O2" = "",
-#          "IL-1" = "",
-#          "JAK-STAT" = "",
-#          "MAPK" = "",
-          "MAPK_PI3K" = "Signaling by EGFR",
-#          "TLR" = "",
-#          "PI3K" = "",
-          "TGFb" = "Signaling by TGF beta",
-#          "TNFa" = "",
+          "IL-1" = "Interleukin-1 signaling",
+          "JAK-STAT" = c("Signaling by Interleukins",
+                         "Interferon Signaling",
+                         "Signalling to STAT3"),
+          "MAPK" = "Signalling to ERKs",
+          "EGFR" = c("Signaling by EGFR",
+                     "Signaling by EGFR in Cancer"),
+          "PI3K" = c("PI3K Cascade",
+                     "Constitutive Signaling by Aberrant PI3K in Cancer",
+                     "PI3K/AKT Signaling in Cancer",
+                     "PI3K/AKT activation"),
+          "TGFb" = "Signaling by TGF-beta Receptor Complex",
+          "TNFa" = "TNF signaling",
           "VEGF" = "Signaling by VEGF",
-          "Wnt" = "Signaling by Wnt",
-          "Insulin" = "Signaling by Insulin receptor"
-#          "Hypoxia" = "",
-#          "RAR" = "",
-#          "p53" = "",
+          "Wnt" = c("Signaling by Wnt",
+                    "Signaling by WNT in cancer"),
+          "Insulin" = "Signaling by Insulin receptor",
+          "Hypoxia" = "Cellular response to hypoxia",
+          "RAR" = "Signaling by Retinoic Acid",
+          "p53" = "Transcriptional Regulation by TP53",
 #          "Estrogen" = "",
-#          "Trail" = "",
-#          "notch" = "",
-#          "NFkB" = "",
-#          "PPAR" = ""
+          "Trail" = "TRAIL  signaling",
+          "notch" = "Signaling by NOTCH",
+          "NFkB" = c("TAK1 activates NFkB by phosphorylation and activation of IKKs complex",
+                     "RIP-mediated NFkB activation via ZBP1"),
+          "PPAR" = c("PPARA activates gene expression",
+                     "Activation of PPARGC1A (PGC-1alpha) by phosphorylation"),
+          "SHH" = "Signaling by Hedgehog"
 )
 
-INFILE = commandArgs(TRUE)[1] %or% "./Enrichr/src/main/resources/Reactome.gmt"
+INFILE = commandArgs(TRUE)[1] %or% "ReactomePathways.gmt"
 OUTFILE = commandArgs(TRUE)[2] %or% "reactome.RData"
 
 lists = er$parse_gmt(INFILE)[unique(unlist(reactome))]
