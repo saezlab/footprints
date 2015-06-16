6ffa8b4953d4ee31b51027c50d88de698726ac6f
mschubert <mschu.dev@gmail.com>
2015-06-16 19:27:00 +0100

    add coread

--- gene_expr/expr.r
+++ gene_expr/expr.r
@@ -16,6 +16,7 @@ avail = icgc$available(clinical=TRUE, rna_seq=TRUE, map_to="icgc_specimen_id")
 clinical = icgc$clinical(avail) %>%
     filter(grepl("Primary", specimen_type)) %>%
     select(icgc_specimen_id, project_code, tissue) %>%
+    mutate(tissue = ifelse(tissue %in% c("COAD","READ"), "COREAD", tissue)) %>%
     filter(tissue %in% tissues)
 tcga_expr = icgc$rna_seq(clinical$icgc_specimen_id, voom=TRUE)
 tcga_expr = tcga_expr[,clinical$icgc_specimen_id] #TODO: duplicates, ordering @icgc module
@@ -35,7 +36,3 @@ corrected = sva::ComBat(expr, batch=batches, mod=mat, par.prior=T)
 
 # save object with matrix for TCGA, GDSC
 io$h5save(corrected, file=OUTFILE)
-
-#TODO: do assocs w/ corrected expr, if not different use this for everything
-#  should also derive clusters from this object (speed2/cluster dir)
-#  maybe: show that batch correction does not influence scores (x vs y plot)
