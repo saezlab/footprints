5aa583cc04609a4388a9ef8cf0ff299af45c452e
mschubert <mschu.dev@gmail.com>
2015-06-29 18:32:17 +0100

    iter fixes

--- analyses/tcga_pathway/tissue_act.r
+++ analyses/tcga_pathway/tissue_act.r
@@ -5,7 +5,7 @@ io = import('io')
 ar = import('array')
 icgc = import('data/icgc')
 
-speed = io$load('../../model/model_norm.RData')
+speed_full = io$load('../../model/model_norm.RData')
 avail = icgc$available(rna_seq=TRUE)
 
 STUDY = c("Breast_Invasive_Carcinoma-TCGA-US",
@@ -16,7 +16,7 @@ STUDY = c("Breast_Invasive_Carcinoma-TCGA-US",
           "Thyroid_Carcinoma-TCGA-US",
           "Neuroblastoma-TARGET-US",
           "Uterine_Corpus_Endometrioid_Carcinoma-TCGA-US",
-        # "Acute_Lymphoblastic_Leukemia-TARGET-US",
+        # "Acute_Lymphoblastic_Leukemia-TARGET-US", # blood cancer, needs different fields
           "Colon_Adenocarcinoma-TCGA-US",
           "Lung_Squamous_Cell_Carcinoma-TCGA-US",
           "Head_and_Neck_Squamous_Cell_Carcinoma-TCGA-US")
@@ -24,18 +24,19 @@ STUDY = c("Breast_Invasive_Carcinoma-TCGA-US",
 pdf("plot.pdf", paper="a4r", width=26, height=20)
 on.exit(dev.off())
 
-for (study in STUDY) {
+for (cur_study in STUDY) {
     # get clinical + expression data
     cc = icgc$clinical() %>%
-        filter(study == study,
+        filter(study == cur_study,
                specimen_type %in% c("Normal - tissue adjacent to primary",
                                     "Normal - solid tissue",
                                     "Primary tumour - solid tissue"),
-               donor_sex == "female",
+#               donor_sex == "female",
                icgc_specimen_id %in% avail) %>%
         select(icgc_donor_id, icgc_specimen_id, specimen_type)
 
     # compute speed scores for all
+    speed = speed_full
     expr = na.omit(icgc$rna_seq(cc$icgc_specimen_id))
     ar$intersect(speed, expr, along=1)
     scores = t(expr) %*% speed %>% ar$map(along=1, scale)
@@ -46,10 +47,9 @@ for (study in STUDY) {
     df$type = cc$specimen_type
     df = melt(df)
 
-    ggplot(df, aes(x=variable, y=value, fill=type)) +
+    print(ggplot(df, aes(x=variable, y=value, fill=type)) +
         geom_boxplot() +
         xlab("pathways") +
         ylab("standard deviations") +
-        ggtitle(paste0(study, ": pathway activation normal vs tumour")) %>%
-        print()
+        ggtitle(paste0(cur_study, ": pathway activation normal vs tumour")))
 }
