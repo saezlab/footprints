1cfd708faa3fd1a2670e7aebf0906cc19d3061dd
mschubert <mschu.dev@gmail.com>
2015-07-15 18:05:16 +0100

    mutation and rppa assocs /w speed scores

---
+++ analyses/tcga_mutations/mut_tissue.r
@@ -0,0 +1,56 @@
+# do the same as with the rppa data:
+# get pathway scores and mutations, and correlate them with each other
+b = import('base')
+io = import('io')
+st = import('stats')
+ar = import('array')
+plt = import('plot')
+tcga = import('data/tcga')
+
+INFILE = "../../scores/tcga/speed_linear.RData"
+OUTFILE = "speed_linear.pdf"
+
+# load expression, RPPA for all cancers where both available
+scores = io$load(INFILE) #TODO: add tissue normals with 0 mutations?
+rownames(scores) = substr(rownames(scores), 1, 16)
+
+mut = tcga$mutations() %>%
+    transmute(hgnc = Hugo_Symbol,
+              sample = substr(Tumor_Sample_Barcode, 1, 16),
+              study = study) %>%
+    group_by(study, hgnc) %>%
+    filter(n() >= 5) %>%
+    ungroup()
+
+pdf("mut_tissue.pdf", paper="a4r", width=26, height=20)
+on.exit(dev.off)
+
+subs2plots = function(subs, mut, scores) {
+    print(subs)
+    m = filter(mut, study==subs)
+    m$mut = 1
+    m = ar$construct(mut ~ sample + hgnc, data=m, fun.aggregate = length) > 0
+    ar$intersect(m, scores)
+
+    # associations
+    result = st$lm(scores ~ m) %>%
+        filter(term == "mTRUE") %>%
+        select(-term) %>%
+        mutate(adj.p = p.adjust(p.value, method="fdr"))
+
+    # matrix plot
+    result %>%
+        mutate(label = ifelse(adj.p < 0.01, "*", "")) %>%
+        plt$cluster(estimate ~ scores + m) %>%
+        filter(adj.p < 0.1) %>%
+        plt$matrix(estimate ~ scores + m, color="estimate") + ggtitle(subs) %>%
+        print()
+
+    # volcano plot
+    result %>%
+        mutate(label = paste(m, scores, sep=":")) %>%
+        plt$color$p_effect(pvalue = "adj.p") %>%
+        plt$volcano(base.size=0.1) + ggtitle(subs) %>%
+        print()
+}
+lapply(unique(mut$study), function(s) subs2plots(s, mut, scores) %catch% NULL)
--- analyses/tcga_rppa/protein_fits.r
+++ analyses/tcga_rppa/protein_fits.r
@@ -1,21 +1,50 @@
+b = import('base')
 io = import('io')
 st = import('stats')
+ar = import('array')
 plt = import('plot')
-icgc = import('data/icgc')
+tcga = import('data/tcga')
 
 INFILE = "../../scores/tcga/speed_linear.RData"
 OUTFILE = "speed_linear.pdf"
 
 # load expression, RPPA for all cancers where both available
-avail = icgc$available(clinical=TRUE, rna_seq=TRUE, rppa=TRUE, map_to="specimen")
-clinical = icgc$clinical(specimen=avail)
-prot = t(icgc$rppa(specimen=avail))
+cc = tcga$clinical() %>%
+    transmute(code = toupper(patient.bcr_patient_barcode),
+              study = study)
+rppa = t(tcga$rppa())
+scores = io$load(INFILE)
+rownames(rppa) = substr(rownames(rppa), 1, 16)
+rownames(scores) = substr(rownames(scores), 1, 16)
+ar$intersect(rppa, scores)
+tissues = cc$study[match(substr(rownames(rppa), 1, 12), cc$code)]
 
-scores = io$load(INFILE)[avail,]
-prot = prot[avail,] # get rid of duplicates
+result = st$lm(scores ~ rppa, subsets=tissues) %>%
+    filter(term == "rppa") %>%
+    select(-term, -std.error, -statistic) %>%
+    group_by(subset) %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+    ungroup()
 
-# plot linear fits w/ covar = exp.series for both, see what difference
-#TODO: linear fits for pathway<-->respective phospho
-mapk = scores[,"MAPK"]
-mek = prot[,"MEK1_pS217_S221"] #/ prot[,"MEK1"]
-plt$linear_fit(mapk ~ mek, subsets=)
+pdf("protein_fits.pdf", paper="a4r", width=26, height=20)
+on.exit(dev.off)
+
+for (subs in unique(result$subset)) {
+    print(subs)
+    r2 = result %>% #TODO: cluster by all tissues, so order is the same?
+        filter(subset == subs) %>%
+        plt$cluster(estimate ~ scores + rppa) %>%
+        group_by(rppa) %>%
+        mutate(p.sum = sum(-log(adj.p))) %>%
+        ungroup() %>%
+        filter(adj.p < 0.05)
+
+    keep = unique(r2[c('rppa','p.sum')]) %>%
+        top_n(40, p.sum)
+
+    r3 = r2 %>%
+        filter(rppa %in% keep$rppa) %>%
+        plt$matrix(estimate ~ scores + rppa, color="estimate") + ggtitle(subs)
+
+    print(r3) %catch% NULL
+}
