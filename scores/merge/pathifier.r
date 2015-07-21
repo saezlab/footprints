a70c114a36d4fb06b4479b951d3dd079028aab82
mschubert <mschu.dev@gmail.com>
2015-07-21 16:13:52 +0100

    separate call from utility

--- scores/merge/pathifier.r
+++ scores/merge/pathifier.r
@@ -1,12 +1,4 @@
-b = import('base')
-io = import('io')
-hpc = import('hpc')
-
-INFILE = commandArgs(TRUE)[1] %or% "../../genesets/reactome.RData"
-EXPR = commandArgs(TRUE)[2] %or% "../../expr_cluster/corrected_expr.h5"
-OUTFILE = commandArgs(TRUE)[3] %or% "pathifier.RData"
-
-tissue2scores = function(tissue, EXPR) {
+tissue2scores = function(tissue, genesets, EXPR) {
     io = import('io')
     pathifier = import_package('pathifier')
 
@@ -26,15 +18,26 @@ tissue2scores = function(tissue, EXPR) {
     )
 }
 
-# load pathway gene sets
-genesets = io$load(INFILE)
+if (is.null(module_name())) {
+    b = import('base')
+    io = import('io')
+    hpc = import('hpc')
 
-# get all tissues which have a normal
-tissues = io$h5load(EXPR, "/tissue")
-tissues = sub("_N", "", unique(tissues[grepl("_N", tissues)]))
+    INFILE = commandArgs(TRUE)[1] %or% "../../genesets/reactome.RData"
+    EXPR = commandArgs(TRUE)[2] %or% "../../expr_cluster/corrected_expr.h5"
+    OUTFILE = commandArgs(TRUE)[3] %or% "pathifier.RData"
 
-# run pathifier in jobs
-result = hpc$Q(tissue2scores, tissue=tissues, more.args=list(EXPR=EXPR), memory=4096)
+    # load pathway gene sets
+    genesets = io$load(INFILE)
 
-# save results
-save(result, file=OUTFILE)
+    # get all tissues which have a normal
+    tissues = io$h5load(EXPR, "/tissue")
+    tissues = sub("_N", "", unique(tissues[grepl("_N", tissues)]))
+
+    # run pathifier in jobs
+    result = hpc$Q(tissue2scores, tissue=tissues,
+        more.args=list(EXPR=EXPR, genesets=genesets), memory=4096)
+
+    # save results
+    save(result, file=OUTFILE)
+}
--- scores/merge/spia.r
+++ scores/merge/spia.r
@@ -1,11 +1,3 @@
-b = import('base')
-io = import('io')
-hpc = import('hpc')
-
-INFILE = commandArgs(TRUE)[1] %or% "../../genesets/reactome.RData"
-EXPR = commandArgs(TRUE)[2] %or% "../../expr_cluster/corrected_expr.h5"
-OUTFILE = commandArgs(TRUE)[3] %or% "spia.RData"
-
 KEGG = list(
     MAPK = "04010", # MAPK signaling pathway
     EGFR = "04012", # ErbB signaling pathway
@@ -22,8 +14,9 @@ KEGG = list(
 )
 
 tissue2scores = function(tissue, lookup, EXPR, KEGG) {
-    b = import('base')
+    library(dplyr)
     io = import('io')
+    ar = import('array')
     spia = import_package('SPIA')
 
     # prepare data
@@ -31,51 +24,71 @@ tissue2scores = function(tissue, lookup, EXPR, KEGG) {
     tumors = t(io$h5load(EXPR, "/expr", index=which(tissues == tissue)))
     normals = t(io$h5load(EXPR, "/expr", index=which(tissues == paste0(tissue, "_N"))))
     data = cbind(tumors, normals)
-    types = c(rep("tumor", ncol(tumors)), rep("normal", ncol(normals)))
 
     # map IDs to entrez gene IDs, SPIA needs this
     rownames(data) = lookup$entrezgene[match(rownames(data), lookup$hgnc_symbol)]
     data = limma::avereps(data[!is.na(rownames(data)),])
 
-    # compute differential expression between tumor and normal
-    design = model.matrix(~ 0 + as.factor(types))
-    colnames(design) = sub("as\\.factor\\(types\\)", "", colnames(design))
-    contrast = limma::makeContrasts("tumor-normal", levels=design)
-    fit = limma::lmFit(data, design) %>%
-        limma::contrasts.fit(contrast) %>%
-        limma::eBayes()
-    result = dplyr::data_frame(gene = rownames(fit),
-        p.value = fit$p.value[,1],
-        fold_change = fit$coefficients[,1]) %>%
-        mutate(adj.p = p.adjust(p.value, method="fdr"))
+    sample2score = function(sample) {
+        # create data for each sample
+        message(sample)
+        data2 = cbind(data[,sample,drop=FALSE], data[,colnames(normals)])
+        types = c("tumor", rep("normal", ncol(normals)))
 
-    # calculate SPIA scores (relevant fields: Name [KEGG name], ID [KEGG ID], tA [score])
-    re = spia$spia(
-        de = setNames(result$fold_change, result$gene)[result$adj.p < 0.05],
-        all = result$gene,
-        organism = "hsa",
-        nB = 2000,
-        plots = FALSE
-    )
-    re = re[re$ID %in% KEGG,]
-    setNames(re$tA, re$Name)
+        # compute differential expression between tumor and normal
+        design = model.matrix(~ 0 + as.factor(types))
+        colnames(design) = sub("as\\.factor\\(types\\)", "", colnames(design))
+        contrast = limma::makeContrasts("tumor-normal", levels=design)
+        fit = limma::lmFit(data2, design) %>%
+            limma::contrasts.fit(contrast) %>%
+            limma::eBayes()
+        result = dplyr::data_frame(gene = rownames(fit),
+            p.value = fit$p.value[,1],
+            fold_change = fit$coefficients[,1]) %>%
+            mutate(adj.p = p.adjust(p.value, method="fdr"))
+
+        # calculate SPIA scores (relevant fields: Name [KEGG name], ID [KEGG ID], tA [score])
+        re = spia$spia(
+            de = setNames(result$fold_change, result$gene)[result$adj.p < 0.05],
+            all = result$gene,
+            organism = "hsa",
+            pathids = unlist(KEGG, use.names=FALSE),
+            nB = 2000, # bootstraps
+            plots = FALSE,
+            verbose = FALSE
+        )
+        setNames(re$tA, re$Name)
+    }
+    
+    sapply(colnames(tumors), sample2score, USE.NAMES=TRUE) %>%
+        ar$stack(along=2)
 }
 
-# load pathway gene sets
-genesets = io$load(INFILE)
+if (is.null(module_name())) {
+    b = import('base')
+    io = import('io')
+    hpc = import('hpc')
 
-# get all tissues which have a normal
-tissues = io$h5load(EXPR, "/tissue")
-tissues = sub("_N", "", unique(tissues[grepl("_N", tissues)]))
+    INFILE = commandArgs(TRUE)[1] %or% "../../genesets/reactome.RData"
+    EXPR = commandArgs(TRUE)[2] %or% "../../expr_cluster/corrected_expr.h5"
+    OUTFILE = commandArgs(TRUE)[3] %or% "spia.RData"
 
-# HGNC -> entrez gene lookup
-lookup = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl") %>%
-    biomaRt::getBM(attributes=c("hgnc_symbol", "entrezgene"),
-    filter="hgnc_symbol", values=io$h5names(EXPR, "/expr")[[2]], mart=.)
+    # load pathway gene sets
+    genesets = io$load(INFILE)
 
-# run pathifier in jobs
-result = hpc$Q(tissue2scores, tissue=tissues,
-    more.args=list(EXPR=EXPR, KEGG=KEGG, lookup=lookup), memory=10240)
+    # get all tissues which have a normal
+    tissues = io$h5load(EXPR, "/tissue")
+    tissues = sub("_N", "", unique(tissues[grepl("_N", tissues)]))
 
-# save results
-save(result, file=OUTFILE)
+    # HGNC -> entrez gene lookup
+    lookup = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl") %>%
+        biomaRt::getBM(attributes=c("hgnc_symbol", "entrezgene"),
+        filter="hgnc_symbol", values=io$h5names(EXPR, "/expr")[[2]], mart=.)
+
+    # run pathifier in jobs
+    result = hpc$Q(tissue2scores, tissue=tissues,
+        more.args=list(EXPR=EXPR, KEGG=KEGG, lookup=lookup), memory=2048)
+
+    # save results
+    save(result, file=OUTFILE)
+}
