8daa14793a4a9254791dc15bc849de9b15f1cb16
mschubert <mschu.dev@gmail.com>
2015-07-29 17:07:31 +0200

    sync .RData+pdf

--- rsync_ebi.sh
+++ rsync_ebi.sh
@@ -2,5 +2,6 @@
 
 cd $(dirname $0)
 
-rsync -auvr --include='*.pdf' --include='*/' --exclude='*' \
+rsync -auvr --include='*.pdf' --include '[zd]scores.RData' --exclude 'data/*' \
+    --include '*.RData' --include='*/' --exclude='*' \
     ebi:/nfs/research2/saezrodriguez/mike/speed2/* . 
