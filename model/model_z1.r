e5a097c1a19c1009f3a9b5c85abf97e0196d260c
mschubert <mschu.dev@gmail.com>
2015-09-08 11:38:32 +0100

    add ||z||-filtered models

---
+++ model/model_z1.r
@@ -0,0 +1,51 @@
+# point of this file:
+# - use the zscores to create a linear model
+library(dplyr)
+b = import('base', attach_operators=FALSE)
+import('base/operators')
+io = import('io')
+ar = import('array')
+st = import('stats')
+
+INFILE = commandArgs(TRUE)[1] %or% "../data/zscores.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "model_z0.RData"
+
+# load speed data, index
+zobj = io$load('../data/zscores.RData')
+zscores = zobj$scores
+index = zobj$index
+
+# adjust object for linear modelling
+inh = index$effect=="inhibiting"
+zscores[,inh] = -zscores[,inh]
+zscores = t(zscores)
+
+# filter by mean abs z-score
+meanz = ar$map(abs(zscores), along=2, function(x) {
+    x = x[!is.na(x)]
+    sum(x) / length(x)
+})
+library(mclust)
+mc = Mclust(meanz)
+keep = mc$classification == 2
+index = index[keep,]
+zscores = zscores[keep,]
+
+# fit model to pathway perturbations
+mod = st$lm(zscores ~ 0 + pathway, data=index, min_pts=100) %>%
+    transmute(gene = zscores,
+              pathway = sub("^pathway", "", term),
+              zscore = estimate,
+              p.value = p.value) %>%
+    group_by(gene) %>%
+    mutate(p.adj = p.adjust(p.value, method="fdr")) %>%
+    ungroup()
+
+zfit = ar$construct(zscore ~ gene + pathway, data=mod)
+pval = ar$construct(p.adj ~ gene + pathway, data=mod)
+
+# filter zfit to only include top 100 genes per pathway
+zfit[apply(pval, 2, p -> !b$min_mask(p, 100))] = 0
+
+# save resulting object
+save(zfit, file=OUTFILE)
