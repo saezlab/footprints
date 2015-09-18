8539cbdea234819c83d11e702f9a2a33895cbbd1
mschubert <mschu.dev@gmail.com>
2015-09-18 18:14:44 +0100

    optimise plots

--- analyses/drug_assocs/drug_assocs.r
+++ analyses/drug_assocs/drug_assocs.r
@@ -3,28 +3,37 @@ b = import('base')
 io = import('io')
 ar = import('array')
 st = import('stats')
-gdsc = import('data/gdsc')
 plt = import('plot')
 
 INFILE = commandArgs(TRUE)[1] %or% "../../scores/gdsc/speed_linear.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "speed_linear.pdf"
+OUTFILE = commandArgs(TRUE)[2] %or% "gdsc/speed_linear.pdf"
 
 # load scores
 scores = io$load(INFILE)
 
 # load sanger data
-Ys = gdsc$drug_response('IC50s') # or AUC
-tissues = gdsc$tissues(minN=5)
-ar$intersect(scores, tissues, Ys, along=1)
+if (grepl("gdsc", INFILE)) {
+    gdsc = import('data/gdsc')
+    Ys = gdsc$drug_response('IC50s') # or AUC
+    Yf = gdsc$drug_response('IC50s', min_tissue_measured=2)
+    tissues = gdsc$tissues(minN=5)
+    ar$intersect(scores, tissues, Ys, Yf, along=1)
+} else if (grepl("ccle", INFILE)) {
+    ccle = import('data/ccle')
+    Ys = ccle$drug_response()
+    Yf = Ys
+    Yf[Ys == 8] = NA
+    tissues = ccle$tissues() #TODO:
+    ar$intersect(scores, tissues, Ys, Yf, along=1)
+}
 
 # save pdf w/ pan-cancer & tissue specific
 pdf(OUTFILE, paper="a4r", width=26, height=20)
-on.exit(dev.off())
 
 # tissues as covariate
 assocs.pan = st$lm(Ys ~ tissues + scores) %>%
     filter(term == "scores") %>%
-    select(-term, -tissues) %>%
+    select(-term) %>%
     mutate(adj.p = p.adjust(p.value, method="fdr"))
 
 # volcano plot for pan-cancer
@@ -42,10 +51,6 @@ assocs.pan %>%
     plt$cluster(lp ~ scores + Ys, size=c(Inf,20)) %>%
     plt$matrix(estimate ~ scores + Ys)
 
-# separate associations for each tissue
-Yf = gdsc$drug_response('IC50s', min_tissue_measured=2)
-ar$intersect(Yf, scores, tissues)
-
 # tissues as subsets
 assocs.tissue = st$lm(Yf ~ scores, subsets=tissues) %>%
     filter(term == "scores") %>%
@@ -56,7 +61,6 @@ assocs.tissue = st$lm(Yf ~ scores, subsets=tissues) %>%
 
 # volcano plot for tissue subsets
 assocs.tissue %>%
-    filter(adj.p < 0.2) %>% # leave out insignificant hits, too many -> file size, etc.
     mutate(label = paste(subset, Yf, scores, sep=":")) %>%
     plt$color$p_effect(pvalue="adj.p", effect="estimate", dir=-1) %>%
     plt$volcano(p=0.2) %>%
@@ -72,3 +76,5 @@ assocs.tissue %>%
 #    plt$matrix(estimate ~ id + Yf)
 
 #TODO: linear fits of significant pan-can assocs
+
+dev.off()
