689c7f3cfe09f8fdcada47b1f5778106c4491195
mschubert <mschu.dev@gmail.com>
2015-09-19 18:29:50 +0100

    split speed cv+heatmaps to see where we lose signal

--- model_nocv/model_linear.r
+++ model_nocv/model_linear.r
@@ -12,7 +12,7 @@ OUTFILE = commandArgs(TRUE)[2] %or% "model_linear.RData"
 
 # load speed data, index; filter for train set only
 zobj = io$load('../data/scores.RData')
-index = zobj$index %>% filter(is.na(exclusion))
+index = zobj$index #%>% filter(is.na(exclusion))
 zscores = zobj$zscores[,index$id]
 
 # adjust object for linear modelling
