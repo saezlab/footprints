f44de8c76808f68273c1898ea1d46d761f898312
mschubert <mschu.dev@gmail.com>
2015-09-25 22:44:04 +0100

    add simple assocs

--- analyses/speed_bootstrap/assocs.r
+++ analyses/speed_bootstrap/assocs.r
@@ -14,7 +14,9 @@ pathway = sign * t(ar$mask(index$pathway)) + 0
 #pathway["TNFa",] = pathway["TNFa",] + pathway["NFkB",]
 
 result = st$lm(scores ~ pathway) %>%
-    mutate(mlogp = -log(p.value))
+    mutate(mlogp = -log(p.value),
+           abseff = abs(estimate),
+           absstat = abs(statistic))
 
 pdf("assocs.pdf", paper="a4r", width=20, height=20)
 
@@ -23,14 +25,13 @@ plt$matrix(result, mlogp ~ scores + pathway, palette="Blues") +
     ylab("Assigned score") +
     ggtitle("- log(p.value)")
 
-#TODO: abs()? -> color scale starts below 0, looks worse than it is
-plt$matrix(result, estimate ~ scores + pathway, palette="Blues") +
+plt$matrix(result, abseff ~ scores + pathway, palette="Blues") +
     xlab("Pathway perturbed") +
     ylab("Assigned score") +
     ggtitle("effect size")
 
 #TODO: figure out what 'statistic' is exactly and how to use it
-plt$matrix(result, statistic ~ scores + pathway, palette="Blues") +
+plt$matrix(result, absstat ~ scores + pathway, palette="Blues") +
     xlab("Pathway perturbed") +
     ylab("Assigned score") +
     ggtitle("statistic")
