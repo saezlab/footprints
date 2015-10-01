dcafcb8d159a437fe001a60b8eb6deef89004cde
mschubert <mschu.dev@gmail.com>
2015-10-01 17:39:58 +0100

    remove GEOD assocs, do speed+path

--- analyses/speed/perturb2expr.r
+++ analyses/speed/perturb2expr.r
@@ -8,58 +8,22 @@ st = import('stats')
 INFILE = commandArgs(TRUE)[1] %or% "../../scores/speed/gsea_reactome.RData"
 OUTFILE = commandArgs(TRUE)[3] %or% "perturb2expr.pdf"
 
-# scale the same way as pheatmap does
-scale_pheatmap = function(mat, n=100, center=TRUE) {
-    color = 1:n
-#    color = colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(n)
+file2assocs = function(fname) {
+    # load data
+    data = io$load(fname)
+    index = data$index
+    scores = data$scores
+    sign = ifelse(index$effect == "activating", 1, -1)
+    pathway = sign * t(ar$mask(index$pathway)) + 0
 
-    # scale matrix
-    x = ar$map(mat, along=1, scale)
-
-    # generate breaks
-    m = max(abs(c(min(x, na.rm = TRUE), max(x, na.rm = TRUE))))
-    breaks = seq(-m, m, length.out = n + 1)
-
-    scaled = color[as.numeric(cut(c(x), breaks=breaks, include.lowest=TRUE))]
-    mat[] = scaled
-    mat
+    # compute associations
+    st$lm(scores ~ pathway)
 }
 
-# load data
-data = io$load(INFILE)
-index = data$index
-scores = scale_pheatmap(data$scores)
-sign = ifelse(index$effect == "activating", 1, -1)
-pathway = sign * t(ar$mask(index$pathway)) + 0
+reactome = file2assocs("../../scores/speed/gsea_reactome.RData") %>% filter(p.value < 1e-4)
+speed = file2assocs("../../scores/speed/speed_matrix.RData") %>% filter(p.value < 1e-4)
 
-# ...
-scores = scores/100 - 0.5
-
-# MAPK-PI3K :: MAPK.E-GEOD.45757
-##hist(scores[index$pathway == "MAPK",'PI3K'])
-#blocked = scores[index$pathway == "MAPK",'PI3K'] < -0.2
-#blocked = names(blocked)[blocked]
-#as.data.frame(index[index$id %in% blocked,])
-MAPK_PI3K = index[index$accession == "E-GEOD-45757",]
-MAPK_PI3K$PI3K = scores[MAPK_PI3K$id,'PI3K']
-MAPK_PI3K = as.data.frame(MAPK_PI3K)
-
-# MAPK-Trail :: MAPK.E-GEOD-35230
-##hist(scores[index$pathway == "MAPK",'Trail'])
-#blocked = scores[index$pathway == "MAPK",'Trail'] < -0.05
-#blocked = names(blocked)[blocked]
-#as.data.frame(index[index$id %in% blocked,])
-MAPK_Trail = index[index$accession == "E-GEOD-35230",]
-MAPK_Trail$Trail = scores[MAPK_Trail$id,'PI3K']
-MAPK_Trail = as.data.frame(MAPK_Trail)
-
-# EGFR-TNFa
-
-# VEGF-JAK-STAT
-
-# compute associations
-result = st$lm(scores ~ pathway) %>%
-    filter(p.value < 0.05) %>%
-    transmute(from=pathway, to=scores, value=effect)
-
-chordDiagram(result, reduce=0.1)
+pdf(OUTFILE, width=12, height=12)
+chordDiagram(transmute(speed, from=pathway, to=scores, value=estimate))
+chordDiagram(transmute(reactome, from=pathway, to=scores, value=estimate))
+dev.off()
