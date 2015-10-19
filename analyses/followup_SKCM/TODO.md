4db0d2fa4b67035ee383594d9cc43242f362cc13
mschubert <mschu.dev@gmail.com>
2015-10-19 17:17:02 +0100

    link VEGF activity + PDGFR expr

---
+++ analyses/followup_SKCM/TODO.md
@@ -0,0 +1,12 @@
+#### Drug associations
+
+10 measured data points min, 1 top hit: VEGF-responsive genes provide resistance to FH-535
+
+#### Gene expression
+
+Look if VEGF is expressed in a subset/all of SKCM cell lines; could use pathway expression
+enrichment scores here.
+
+#### LINCS check
+
+Treating with a VEGF inhibitor, does the VEGF signature decrease?
--- analyses/followup_SKCM/plot.r
+++ analyses/followup_SKCM/plot.r
@@ -20,28 +20,35 @@ assocfile2plot = function(fname) {
         plt$volcano(p=0.2)
 }
 
+do_fit = function(tissue, drug, path, gene) {
+    fits = df$assemble(
+        drug = speed$Yf[,drug],
+        path = speed$scores[,path],
+        muts = gdsc$mutated_genes(intogen=TRUE, tissue=tissue) %>%
+            ar$map(along=2, function(x) paste(names(x)[x], collapse=",")),
+        gene = gdsc$basal_expression()[gene,]
+    )
+    fits$drug = as.numeric(fits$drug)
+    fits$path = as.numeric(fits$path)
+    fits$gene = as.numeric(fits$gene)
+
+    ggplot(fits, aes(x=path, y=drug, label=muts, fill=gene)) +
+        geom_smooth(method=stats::lm, se=FALSE, na.rm=TRUE, alpha=0.1) +
+        geom_point(pch=21, size=5, colour="black") +
+        scale_fill_gradient(low="white", high="black",
+                            limits=c(min(fits$gene, na.rm=TRUE),
+                                     max(fits$gene, na.rm=TRUE)),
+                            guide = guide_legend(title = gene)) +
+        theme_bw() +
+        ggtitle("SKCM: FH535 response and VEGF activity") +
+        xlab("predicted VEGF activity") +
+        ylab("log IC50 FH535 [beta-catenin]")
+}
+
 speed = io$load("speed_matrix.RData")
 
 pdf(OUTFILE, paper="a4r", width=26, height=20)
-print(assocfile2plot("speed_matrix.RData") + ggtitle("response genes"))
-#print(assocfile2plot("gsea_reactome.RData") + ggtitle("reactome expr"))
-
-#TODO: write this: take named vectors and sort in df
-fits = df$assemble(
-    FH535 = speed$Yf[,"FH535"],
-    VEGF = speed$scores[,"VEGF"],
-    muts = gdsc$mutated_genes(intogen=TRUE, tissue="SKCM") %>%
-        ar$map(along=2, function(x) paste(names(x)[x], collapse=",")),
-    FLT1 = gdsc$basal_expression()["FLT1",] # VEGFR1
-)
-fits$FH535 = as.numeric(fits$FH535)
-fits$VEGF = as.numeric(fits$VEGF)
-fits$FLT1 = as.numeric(fits$FLT1)
-
-ggplot(fits, aes(x=VEGF, y=FH535, label=muts, fill=FLT1)) +
-    geom_smooth(method=stats::lm, se=FALSE, na.rm=TRUE, alpha=0.1) +
-    geom_point(pch=21, size=3, colour="black") #+
-# there is not much info in mutations, FLT1 expression
-#    geom_text(aes(size=0.2))
-# colour by vegf expression?
+print(assocfile2plot(INFILE))
+print(do_fit("SKCM", "FH535", "VEGF", "PDGFRA"))
+print(do_fit("SKCM", "FH535", "VEGF", "PDGFRB"))
 dev.off()
