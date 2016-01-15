364bf3866dbffff8ca4f7b634ff3ef3f4385fd7d
mschubert <mschu.dev@gmail.com>
2016-01-15 16:41:09 +0000

    sync figures back

--- rsync_ebi.sh
+++ rsync_ebi.sh
@@ -5,3 +5,5 @@ cd $(dirname $0)
 rsync -auvr --include='*.pdf' --include '[zd]scores.RData' --exclude 'data/*' \
     --include '*.RData' --include='*/' --exclude='*' \
     ebi:/nfs/research2/saezrodriguez/mike/speed2/* . 
+
+rsync -avur figure/* ebi:/nfs/research2/saezrodriguez/mike/speed2/figure/
