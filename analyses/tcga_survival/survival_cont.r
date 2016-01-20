6b10ba0229c526c054e02b101212c8426f3e698c
mschubert <mschu.dev@gmail.com>
2016-01-20 13:32:08 +0000

    default speed_mat, cleanup

--- analyses/tcga_survival/survival_cont.r
+++ analyses/tcga_survival/survival_cont.r
@@ -7,8 +7,8 @@ plt = import('plot')
 tcga = import('data/tcga')
 util = import('./util')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../scores/tcga/speed_norm.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "cont_speed_norm.pdf"
+INFILE = commandArgs(TRUE)[1] %or% "../../scores/tcga/speed_matrix.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "cont_speed_matrix.pdf"
 
 # load scores, only select primary tumors & map to patient IDs
 scores = io$load(INFILE)
@@ -18,14 +18,12 @@ rownames(scores) = substr(rownames(scores), 1, 12)
 pdf(OUTFILE, paper="a4r", width=26, height=20)
 on.exit(dev.off)
 
-#    plt$color$p_effect("p.value", dir=-1) %>%
 util$pancan(scores) %>%
     plt$color$p_effect("adj.p", dir=-1, thresh=0.1) %>%
     mutate(label = scores) %>%
     plt$volcano(base.size=0.1, p=0.1) %>%
     print()
 
-#    plt$color$p_effect("p.value", dir=-1, thresh=0.1) %>%
 util$tissue(scores) %>%
     plt$color$p_effect("adj.p", dir=-1, thresh=0.1) %>%
     mutate(label = paste(subset, scores, sep=":")) %>%
