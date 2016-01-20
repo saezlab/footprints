ebefc8c44ab5ac1172cff6683576faa2aea9a016
mschubert <mschu.dev@gmail.com>
2016-01-20 13:16:40 +0000

    add comments, don't filter treated

--- analyses/tcga_survival/survival_cont.r
+++ analyses/tcga_survival/survival_cont.r
@@ -18,12 +18,14 @@ rownames(scores) = substr(rownames(scores), 1, 12)
 pdf(OUTFILE, paper="a4r", width=26, height=20)
 on.exit(dev.off)
 
+#    plt$color$p_effect("p.value", dir=-1) %>%
 util$pancan(scores) %>%
     plt$color$p_effect("adj.p", dir=-1) %>%
     mutate(label = scores) %>%
     plt$volcano(base.size=0.1) %>%
     print()
 
+#    plt$color$p_effect("p.value", dir=-1, thresh=0.1) %>%
 util$tissue(scores) %>%
     plt$color$p_effect("adj.p", dir=-1, thresh=0.1) %>%
     mutate(label = paste(subset, scores, sep=":")) %>%
