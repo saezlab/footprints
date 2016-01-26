9c1c655ba237bb2e4f28b1d0aa975c495f64c372
mschubert <mschu.dev@gmail.com>
2016-01-26 16:04:33 +0000

    rework tcga/gdsc batch correction

--- util/expr_cluster/expr.r
+++ util/expr_cluster/expr.r
@@ -6,39 +6,48 @@ st = import('stats')
 gdsc = import('data/gdsc')
 tcga = import('data/tcga')
 
-OUTFILE = commandArgs(TRUE)[1] %or% "corrected_expr.h5"
+OUTFILE = commandArgs(TRUE)[1] %or% "corrected_expr.RData"
+
+# specify the tissues that we want
+include_tissues = import('../../config')$tcga$tissues_with_normals
 
 # load GDSC data
-gdsc_expr = t(gdsc$basal_expression())
-gdsc_tissues = gdsc$tissues(minN=10)
-ar$intersect(gdsc_expr, gdsc_tissues, along=1)
-gdsc_expr = t(gdsc_expr)
+gdsc_expr = gdsc$basal_expression()
+gdsc_index = data_frame(id=colnames(gdsc_expr)) %>%
+    mutate(study = "gdsc",
+           type = "Cancer Cell Line",
+           tissue = gdsc$cosmic$id2tissue(id)) %>%
+    mutate(covar = tissue,
+           batch = "gdsc_cell_line") %>%
+    filter(tissue %in% include_tissues | tissue == "COREAD") %>%
+    distinct()
 
 # load TCGA data
-tcga_expr = ar$stack(lapply(tcga$tissues(), tcga$rna_seq), along=2)
+tcga_expr = ar$stack(lapply(include_tissues, tcga$rna_seq), along=2)
 tcga_index = tcga$barcode2index(colnames(tcga_expr)) %>%
     transmute(id = Bio.ID,
               study = Study.Abbreviation,
               type = Sample.Definition) %>%
-    mutate(tissue = ifelse(study %in% c("COAD","READ"), "COREAD", study),
-           covar = paste0(tissue, ifelse(type=="Solid Tissue Normal", "_N", ""))) %>%
-    filter(tissue %in% unique(gdsc_tissues))
+    mutate(tissue = ifelse(study %in% c("COAD","READ","COADREAD"), "COREAD", study),
+           covar = paste0(tissue, ifelse(type=="Solid Tissue Normal", "_normal", "")),
+           batch = study) %>%
+    distinct()
 
-# intersect tissues and subset matrices/index
+# assemble data objects
 tcga_expr = tcga_expr[,tcga_index$id]
-gdsc_expr = gdsc_expr[,gdsc_tissues %in% unique(tcga_index$tissue)]
-gdsc_tissues = gdsc_tissues[gdsc_tissues %in% unique(tcga_index$tissue)]
-
-# define covariates and batches
-batches = c(tcga_index$study, rep("cell_line", ncol(gdsc_expr)))
-covar = unname(c(tcga_index$covar, gdsc_tissues))
-
-# perform batch correction
+gdsc_expr = gdsc_expr[,gdsc_index$id]
 ar$intersect(tcga_expr, gdsc_expr, along=1)
-expr = cbind(tcga_expr, gdsc_expr)
-mat = model.matrix(~as.factor(covar))
+raw_expr = cbind(tcga_expr, gdsc_expr)
+index = bind_rows(tcga_index, gdsc_index)
+
+# make sure we don't have duplicates and ids match
+stopifnot(sum(duplicated(index$id)) == 0)
+stopifnot(index$id == colnames(raw_expr))
+
+# create model matrix and perform batch correction
+mat = model.matrix(~as.factor(index$covar))
 set.seed(1562681)
-corrected = sva::ComBat(expr, batch=batches, mod=mat, par.prior=TRUE)
+expr = sva::ComBat(raw_expr, batch=index$batch, mod=mat, par.prior=TRUE)
 
 # save object with matrix for TCGA, GDSC
-io$h5save(list(expr=t(corrected), tissue=covar), file=OUTFILE)
+save(expr, index, file=OUTFILE)
