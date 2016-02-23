c3e8a6fcff618d7ecbd6b892b268af4d5186fff6
mschubert <mschu.dev@gmail.com>
2016-02-23 10:44:32 +0000

    rename seed var to _offset (ebits/#85)

--- analyses/speed_bootstrap/model_resample.r
+++ analyses/speed_bootstrap/model_resample.r
@@ -2,9 +2,9 @@ b = import('base')
 io = import('io')
 hpc = import('hpc')
 
-calc_resample = function(zdata, seed) {
+calc_resample = function(zdata, seed_offset) {
     library(dplyr)
-    set.seed(1059371 + seed)
+    set.seed(1059371 + seed_offset)
 
     # resample the index and z objects
     index = zdata$index %>%
@@ -23,12 +23,11 @@ calc_resample = function(zdata, seed) {
 
 ZDATA = commandArgs(TRUE)[1] %or% '../../data/zscores.RData'
 OUTFILE = commandArgs(TRUE)[2] %or% 'model_resample.RData'
-RESAMPLE = 1000
 
 # start from the calculated zscores.RData in ../../data
 zdata = io$load(ZDATA)
-result = hpc$Q(calc_resample, seed=1:RESAMPLE, const=list(zdata=zdata),
-               n_jobs=RESAMPLE, memory=2048)
+result = hpc$Q(calc_resample, seed_offset=1:1000, const=list(zdata=zdata),
+               n_jobs=1000, memory=2048)
 #result = calc_resample(zdata, 0)
 
 # save model resulting from resample in model/ dir
