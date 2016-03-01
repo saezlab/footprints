d499ea646bc06d9071831bfa014526ad7b83e61e
mschubert <mschu.dev@gmail.com>
2016-03-01 11:12:39 +0000

    go 5<=n<=100 genes in set

--- util/genesets/all/go.r
+++ util/genesets/all/go.r
@@ -4,16 +4,18 @@ library(dplyr)
 b = import('base')
 
 OUTFILE = commandArgs(TRUE)[1] %or% "go.RData"
+MIN_GENES = 5
+MAX_GENES = 100
 
 # 319k x2 for all genes, 20k x2 for landmarks
 mart = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
 
 mapGO = biomaRt::getBM(attributes=c("hgnc_symbol", "go_id", "name_1006"), mart=mart)
 
-#TODO: filter by number of genes, name them with ID+name
-
 stacked = mapGO %>%
     filter(go_id != "") %>%
+    group_by(go_id) %>%
+    filter(n()>=MIN_GENES & n()<=MAX_GENES) %>%
     mutate(go=paste(go_id,name_1006)) %>%
     select(hgnc_symbol,go_id)
 
