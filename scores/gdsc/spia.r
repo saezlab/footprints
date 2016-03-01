6a4e60243d7337c903e54c39657545e824f43d8b
mschubert <mschu.dev@gmail.com>
2016-03-01 12:30:24 +0000

    new make structure; spia

--- scores/gdsc/spia.r
+++ scores/gdsc/spia.r
@@ -13,10 +13,10 @@ hpc = import('hpc')
 #' @param spia     A loaded `spia` module to keep the paths form master
 #'                 (this should not be required with zmq `hpc` module)
 #' @return         TODO
-sample2scores = function(sample, expr, tissues, spia) {
+sample2scores = function(sample, expr, tissues, spia, pathids=NULL) {
     sample_tissue = tissues[sample]
     other_tissue = setdiff(names(tissues)[tissues == sample_tissue], sample)
-    spia$spia(sample, other_tissue, data=expr, pathids=spia$speed2kegg)
+    spia$spia(sample, other_tissue, data=expr, pathids=pathids)
 }
 
 #' Maps row names from HGNC symbols to Entrez Gene IDs
@@ -34,6 +34,7 @@ map_entrez = function(expr) {
 
 if (is.null(module_name())) {
     OUTFILE = commandArgs(TRUE)[1] %or% "spia.RData"
+    FILTER = as.logical(commandArgs(TRUE)[2]) %or% TRUE
 
     # load pathway gene sets and tissues
     expr = gdsc$basal_expression()
@@ -43,15 +44,21 @@ if (is.null(module_name())) {
 
     expr = map_entrez(t(expr))
 
+    if (FILTER)
+        pathids = spia$speed2kegg
+    else
+        pathids = NULL
+
     # run spia in jobs and save
     result = hpc$Q(sample2scores, sample=colnames(expr),
-                   const=list(expr=expr, tissues=tissues, spia=spia),
-                   memory=8192, n_jobs=30) %>%
+                   const=list(expr=expr, tissues=tissues, spia=spia, pathids=pathids),
+                   memory=8192, job_size=20) %>%
         setNames(colnames(expr)) %>%
         ar$stack(along=1) %>%
         ar$map(along=1, scale)
 
-    colnames(result) = spia$kegg2speed[colnames(result)]
+    if (FILTER)
+        colnames(result) = spia$kegg2speed[colnames(result)]
 
     save(result, file=OUTFILE)
 }
