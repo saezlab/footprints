c72bf690d1a9bfc71708d9251b1d17f7898b8b0f
mschubert <mschu.dev@gmail.com>
2016-03-01 12:06:24 +0000

    submit job to handle large num of sets

--- scores/gdsc/set_enrichment.r
+++ scores/gdsc/set_enrichment.r
@@ -2,15 +2,21 @@ b = import('base')
 io = import('io')
 gdsc = import('data/gdsc')
 gsea = import('../../util/gsea')
+hpc = import('hpc')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/go.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "gsea_go.RData"
+INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/mapped/go.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "pathways_mapped/gsea_go.RData"
 
 # load gene list and expression
 genelist = io$load(INFILE)
 expr = gdsc$basal_expression()
 
 # perform GSEA
-result = gsea$runGSEA(expr, genelist, transform.normal=TRUE)
+result = hpc$Q(gsea$runGSEA, sigs=genelist,
+               const = list(expr=expr, transform.normal=TRUE),
+               memory = 2048, n_jobs = ceiling(length(genelist)/50)
+
+result = setNames(result, names(genelist)) %>%
+    ar$stack(along=2)
 
 save(result, file=OUTFILE)
