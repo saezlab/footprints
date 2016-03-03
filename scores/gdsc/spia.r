552eb3c85ff4d4809f1693eec435c5d6fb01fabc
mschubert <mschu.dev@gmail.com>
2016-03-03 17:24:52 +0000

    refactor job distribution

--- scores/gdsc/spia.r
+++ scores/gdsc/spia.r
@@ -1,11 +1,11 @@
 b = import('base')
 io = import('io')
 ar = import('array')
+df = import('data_frame')
 spia = import('../../util/spia')
 gdsc = import('data/gdsc')
-hpc = import('hpc')
 
-OUTFILE = commandArgs(TRUE)[1] %or% "spia.RData"
+OUTFILE = commandArgs(TRUE)[1] %or% "pathways_mapped/spia.RData"
 FILTER = as.logical(commandArgs(TRUE)[2]) %or% TRUE
 
 #' Calculates SPIA scores for one sample vs all other tissues
@@ -13,15 +13,12 @@ FILTER = as.logical(commandArgs(TRUE)[2]) %or% TRUE
 #' @param sample   A character ID of the sample to compute scores for
 #' @param expr     An expression matrix with [genes x samples]
 #' @param tissues  A named (COSMIC ID) vector of TCGA tissues
-#' @param spia     A loaded `spia` module to keep the paths form master
-#'                 (this should not be required with zmq `hpc` module)
 #' @return         TODO
-sample2scores = function(sample, expr, tissues, spia, pathids=NULL) {
-#    b = import('base')
+sample2scores = function(sample, expr, tissues, pathids=NULL) {
 	spia = import('../../util/spia')
     sample_tissue = tissues[sample]
-    other_tissue = setdiff(names(tissues)[tissues == sample_tissue], sample)
-    spia$spia(sample, other_tissue, data=expr, pathids=pathids) #%catch% NA #TODO: shld b cvd by f_o_e
+    same_tissue = setdiff(names(tissues)[tissues == sample_tissue], sample)
+    unname(spia$spia(sample, same_tissue, data=expr, pathids=pathids))
 }
 
 # load pathway gene sets and tissues
@@ -31,24 +28,18 @@ expr = t(expr) # this should work with along=-1
 ar$intersect(tissues, expr, along=1)
 
 expr = spia$map_entrez(t(expr))
-
 if (FILTER) {
-    genesets = spia$speed2kegg
+    pathids = spia$speed2kegg
 } else {
-    genesets = spia$pathids("hsa")
+    pathids = spia$pathids("hsa")
 }
 
-# make compatible to call with one set in above function
-for (i in seq_along(genesets))
-    genesets[[i]] = setNames(list(genesets[[i]]), names(genesets)[i])
-
 # run spia in jobs and save
-result = hpc$Q(sample2scores, sample=colnames(expr), pathids=genesets,
-               const=list(expr=expr, tissues=tissues), expand_grid=TRUE,
-               memory=8192, job_size=50, fail_on_error=FALSE)
+result = b$expand_grid(sample = colnames(expr), pathids = pathids) %>%
+    df$call(sample2scores, expr = expr, tissues = tissues,
+            hpc_args = list(memory=4096, job_size=100, fail_on_error=FALSE))
 
-result[sapply(result, class) == "try-error"] = NA
-result = ar$stack(result, along=2)
+result = ar$construct(result ~ sample + pathids, result)
 
 if (FILTER)
     colnames(result) = spia$kegg2speed[colnames(result)]
