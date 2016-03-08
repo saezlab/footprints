2abdf297f7d9bc64f618597e2f30e019b648390e
mschubert <mschu.dev@gmail.com>
2016-03-08 13:53:58 +0000

    make import-able

--- analyses/speed_overview/plot_bars.r
+++ analyses/speed_overview/plot_bars.r
@@ -1,20 +1,26 @@
-library(cowplot)
-library(reshape2)
-io = import('io')
+do_plot = function() {   
+    library(cowplot)
+    library(reshape2)
+    io = import('io')
 
-nums = read.table("numbers.txt", header=TRUE)
-nums$speed2 = NULL
-nums = melt(nums, id="num")
-nums$num = factor(nums$num, levels=c("pathways", "experiments", "contrasts", "arrays"))
+    nums = read.table(module_file("numbers.txt"), header=TRUE)
+    nums$speed2 = NULL
+    nums = melt(nums, id="num")
+    nums$num = factor(nums$num, levels=c("pathways", "experiments", "contrasts", "arrays"))
 
-pdf("bars.pdf")
+    ggplot(nums, aes(x=variable, y=value, fill=variable)) +
+        geom_bar(stat="identity") +
+        facet_wrap(~ num, scales="free") +
+        xlab("Signature Method") +
+        ylab("Number") +
+        theme(axis.text.x = element_text(angle=45, hjust=1),
+              legend.position = "none")
+}
 
-ggplot(nums, aes(x=variable, y=value, fill=variable)) +
-    geom_bar(stat="identity") +
-    facet_wrap(~ num, scales="free") +
-    xlab("Signature Method") +
-    ylab("Number") +
-    theme(axis.text.x = element_text(angle=45, hjust=1),
-          legend.position = "none")
+if (is.null(module_name())) {
+    pdf("bars.pdf")
 
-dev.off()
+    do_plot()
+
+    dev.off()
+}
--- analyses/speed_raw/plot.r
+++ analyses/speed_raw/plot.r
@@ -1,28 +1,35 @@
-library(dplyr)
-library(cowplot)
-io = import('io')
+do_plot = function() {    
+    library(dplyr)
+    library(cowplot)
+    io = import('io')
 
-OUTFILE = "plot.pdf"
+    OUTFILE = "plot.pdf"
 
-load_fun = function(fname, name) {
-    re = io$load(fname) %>%
-        select(id, pathway, x, y)
-    re$method = name
-    re
+    load_fun = function(fname, name) {
+        re = io$load(module_file(fname)) %>%
+            select(id, pathway, x, y)
+        re$method = name
+        re
+    }
+
+    df = bind_rows(load_fun('tsne_fc.RData', "fold change"),
+                   load_fun('tsne_fullmat.RData', "no filter"),
+                   load_fun('tsne_top100.RData', "top 100 genes"))
+
+    ggplot(df, aes(x=x, y=y, color=pathway)) +
+        geom_point() +
+        facet_wrap(~method) +
+        xlab("A.U.") +
+        ylab("A.U.") +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_blank())
+
 }
 
-df = bind_rows(load_fun('tsne_fc.RData', "fold change"),
-               load_fun('tsne_fullmat.RData', "no filter"),
-               load_fun('tsne_top100.RData', "top 100 genes"))
+if (is.null(module_name())) {
+    pdf(OUTFILE, paper="a4r", width=10, height=4)
 
-pdf(OUTFILE, paper="a4r", width=10, height=4)
+    do_plot()
 
-ggplot(df, aes(x=x, y=y, color=pathway)) +
-    geom_point() +
-    facet_wrap(~method) +
-    xlab("A.U.") +
-    ylab("A.U.") +
-    theme(axis.text.x = element_blank(),
-          axis.text.y = element_blank())
-
-dev.off()
+    dev.off()
+}
