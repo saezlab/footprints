dfa4a3d32e59d77b9ce5f8e968b66c4a26ff9740
mschubert <mschu.dev@gmail.com>
2016-03-26 13:26:46 +0000

    streamline drug assocs

--- analyses/drug_assocs/gdsc_assocs.r
+++ analyses/drug_assocs/gdsc_assocs.r
@@ -14,8 +14,9 @@ Ys = gdsc$drug_response('IC50s') # or AUC
 Ys_clinical = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2)
 Ys_noexp = gdsc$drug_response('IC50s', min_tissue_measured=0, median_top=10, stage=1)
 Ys_sensi = gdsc$drug_response('IC50s', min_tissue_measured=5, median_top=10)
+Ys_clin_sens = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2, median_top=10)
 tissues = gdsc$tissues(minN=15)
-ar$intersect(scores, tissues, Ys, Ys_clinical, Ys_noexp, Ys_sensi, along=1)
+ar$intersect(scores, tissues, Ys, Ys_clinical, Ys_noexp, Ys_sensi, Ys_clin_sens, along=1)
 
 # tissues as covariate
 assocs.pan = st$lm(Ys ~ tissues + scores) %>%
@@ -34,5 +35,6 @@ assocs.tissue = function(Yf) st$lm(Yf ~ scores, subsets=tissues) %>%
 assocs.tissue_clinical = assocs.tissue(Ys_clinical)
 assocs.tissue_noexp = assocs.tissue(Ys_noexp)
 assocs.tissue_sensi = assocs.tissue(Ys_sensi)
+assocs.tissue_clin_sens = assocs.tissue(Ys_clin_sens)
 
-save(assocs.pan, assocs.tissue_clinical, assocs.tissue_noexp, assocs.tissue_sensi, file=OUTFILE)
+save(assocs.pan, assocs.tissue_clinical, assocs.tissue_noexp, assocs.tissue_sensi, assocs.tissue_clin_sens, file=OUTFILE)
