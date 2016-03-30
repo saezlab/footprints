f3f3e87b1762600a8bfa15676b8445319af21208
mschubert <mschu.dev@gmail.com>
2016-03-30 11:28:14 +0100

    better way of job splitting

--- analyses/drug_assocs/gdsc_assocs.r
+++ analyses/drug_assocs/gdsc_assocs.r
@@ -2,39 +2,79 @@ library(dplyr)
 b = import('base')
 io = import('io')
 ar = import('array')
-st = import('stats')
+hpc = import('hpc')
 gdsc = import('data/gdsc')
 
 INFILE = commandArgs(TRUE)[1] %or% "../../scores/gdsc/pathways_mapped/speed_linear.RData"
 OUTFILE = commandArgs(TRUE)[2] %or% "speed_linear.RData"
 
 # load required data
-scores = io$load(INFILE)
-Ys = gdsc$drug_response('IC50s') # or AUC
-Ys_clinical = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2)
-Ys_noexp = gdsc$drug_response('IC50s', min_tissue_measured=0, median_top=10, stage=1)
-Ys_sensi = gdsc$drug_response('IC50s', min_tissue_measured=5, median_top=10)
-Ys_clin_sens = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2, median_top=10)
-tissues = gdsc$tissues(minN=15)
-ar$intersect(scores, tissues, Ys, Ys_clinical, Ys_noexp, Ys_sensi, Ys_clin_sens, along=1)
+data = list(
+    scores = io$load(INFILE),
+    Ys = gdsc$drug_response('IC50s'),
+    clinical = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2),
+    noexp = gdsc$drug_response('IC50s', min_tissue_measured=0, median_top=10, stage=1),
+    sensi = gdsc$drug_response('IC50s', min_tissue_measured=5, median_top=10),
+    clin_sens = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2, median_top=10),
+    tissues = gdsc$tissues(minN=15)
+) %>% ar$intersect_list(along=1)
 
-# tissues as covariate
-assocs.pan = st$lm(Ys ~ tissues + scores, hpc_args=list(job_size=2000)) %>%
-    filter(term == "scores") %>%
-    select(-term) %>%
+scores = data$scores
+tissues = data$tissues
+Ys = data$Ys
+Ysubs = data[c('clinical', 'noexp', 'sensi', 'clin_sens')]
+
+#' Tissues as covariate
+#'
+#' @param scores    A matrix of [cell lines x set or pathway scores]
+#' @param Ys        A matrix drug response [cell lines x drugs]
+#' @param scoresub  A character string of which column of `scores` to subset
+#' @param tissues   A character vector of tissues, same cell lines as `scores`
+#' @return          A data.frame with the association results
+pan_assocs = function(scores, Ys, scoresub, tissues) {
+    ar = import('array')
+    st = import('stats')
+
+    score = scores[, scoresub, drop=FALSE]
+
+    assocs.pan = st$lm(Ys ~ tissues + score, data=data) %>%
+        filter(term == "score") %>%
+        select(-term)
+}
+
+#' Tissues as subsets
+#'
+#' @param scores    A matrix of [cell lines x set or pathway scores]
+#' @param Ysubs     A list of different subsets for drug response
+#' @param scoresub  A character string of which column of `scores` to subset
+#' @param ysub      A character string of which `Ysubs` to subset
+#' @param tissues   A character vector of tissues, same cell lines as `scores`
+#' @return          A data.frame with the association results
+tissue_assocs = function(scores, Ysubs, scoresub, ysub, tissues) {
+    ar = import('array')
+    st = import('stats')
+
+    data = list(Yf = Ysubs[[ysub]], score = scores[,scoresub,drop=FALSE]) %>%
+        ar$intersect_list(along=1)
+
+    re = st$lm(Yf ~ score, subsets=tissues, data=data) %>%
+        filter(term == "score") %>%
+        select(-term) %>%
+        mutate(Ysub = ysub)
+}
+
+assocs.pan = hpc$Q(pan_assocs, scoresub=colnames(scores),
+                   const = list(scores=scores, Ys=Ys, tissues=tissues),
+                   job_size = 100) %>%
+    bind_rows() %>%
     mutate(adj.p = p.adjust(p.value, method="fdr"))
 
-# tissues as subsets
-assocs.tissue = function(Yf) st$lm(Yf ~ scores, subsets=tissues, hpc_args=list(job_size=10000)) %>%
-    filter(term == "scores") %>%
-    select(-term) %>%
-    group_by(subset) %>%
+assocs.tissue = hpc$Q(tissue_assocs, scoresub=colnames(scores), ysub=names(Ysubs),
+                      const = list(scores=scores, Ysubs=Ysubs, tissues=tissues),
+                      expand_grid=TRUE, job_size=100) %>%
+    bind_rows() %>%
+    group_by(subset, Ysub) %>%
     mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
     ungroup()
 
-assocs.tissue_clinical = assocs.tissue(Ys_clinical)
-assocs.tissue_noexp = assocs.tissue(Ys_noexp)
-assocs.tissue_sensi = assocs.tissue(Ys_sensi)
-assocs.tissue_clin_sens = assocs.tissue(Ys_clin_sens)
-
-save(assocs.pan, assocs.tissue_clinical, assocs.tissue_noexp, assocs.tissue_sensi, assocs.tissue_clin_sens, file=OUTFILE)
+save(assocs.pan, assocs.tissue, file=OUTFILE)
