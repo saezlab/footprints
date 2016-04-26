29eced92d255209976685a53649993a28e1d1161
mschubert <mschu.dev@gmail.com>
2016-04-26 16:19:02 +0100

    use same plots for discr+cont volcanos

--- analyses/tcga_survival/assocs_cont.r
+++ analyses/tcga_survival/assocs_cont.r
@@ -7,16 +7,13 @@ plt = import('plot')
 tcga = import('data/tcga')
 surv = import('./util')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../scores/tcga/speed_matrix.RData"
+INFILE = commandArgs(TRUE)[1] %or% "../../scores/tcga/pathways_mapped/speed_matrix.RData"
 OUTFILE = commandArgs(TRUE)[2] %or% "cont_speed_matrix.RData"
 
 # load scores, only select primary tumors & map to patient IDs
 scores = surv$load(file=INFILE)
 
-pdf(OUTFILE, paper="a4r", width=26, height=20)
-on.exit(dev.off)
-
-pancan = surv$pancan(scores)
+pan_cov = surv$pancan(scores)
 tissue = surv$tissue(scores)
 
-save(pancan, tissue, file=OUTFILE)
+save(pan_cov, tissue, file=OUTFILE)
--- analyses/tcga_survival/plot.r
+++ analyses/tcga_survival/plot.r
@@ -7,22 +7,22 @@ plt = import('plot')
 tcga = import('data/tcga')
 surv = import('./util')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../scores/tcga/speed_matrix.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "cont_speed_matrix.pdf"
+INFILE = commandArgs(TRUE)[1] %or% "assocs_cont_mapped/speed_matrix.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "plots_cont_mapped/speed_matrix.pdf"
 
 # load scores, only select primary tumors & map to patient IDs
-scores = surv$load(file=INFILE)
+assocs = io$load(file=INFILE)
 
 pdf(OUTFILE, paper="a4r", width=26, height=20)
 on.exit(dev.off)
 
-surv$pancan(scores) %>%
+assocs$pan_cov %>%
     plt$color$p_effect("adj.p", dir=-1, thresh=0.1) %>%
     mutate(label = scores) %>%
     plt$volcano(base.size=0.1, p=0.1) %>%
     print()
 
-surv$tissue(scores) %>%
+assocs$tissue %>%
     plt$color$p_effect("adj.p", dir=-1, thresh=0.1) %>%
     mutate(label = paste(subset, scores, sep=":")) %>%
     plt$volcano(p=0.1) %>% #+ ggtitle(sum(clinical$adj.p < 0.1)) %>%
