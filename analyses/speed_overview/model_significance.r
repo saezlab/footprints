47f47e81968441c81527424b3ed02d2ed8108acf
mschubert <mschu.dev@gmail.com>
2016-05-24 16:52:36 +0100

    add model significance

---
+++ analyses/speed_overview/model_significance.r
@@ -0,0 +1,10 @@
+library(dplyr)
+io = import('io')
+
+assocs = io$load('../../model/model_matrix.RData')$assocs
+
+speed = io$load('../../model/model_matrix.RData')$assocs %>%
+    group_by(pathway) %>%
+    top_n(100, -p.value) %>%
+	summarize(avg_fdr = mean(p.adj)) %>%
+	print()
