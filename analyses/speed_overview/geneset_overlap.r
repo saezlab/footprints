18827531a24321df7a977fb1a099a3b7ca1e25ca
mschubert <mschu.dev@gmail.com>
2016-05-24 16:25:28 +0100

    add venn plots

---
+++ analyses/speed_overview/geneset_overlap.r
@@ -0,0 +1,38 @@
+library(dplyr)
+library(grid)
+library(gridExtra)
+b = import('base')
+io = import('io')
+ar = import('array')
+plt = import('plot')
+
+# want: a list for GO/Reactome/SPEED, pathway, gene
+go = io$load('../../util/genesets/mapped/go.RData')
+reactome = io$load('../../util/genesets/mapped/reactome.RData')
+speed = io$load('../../model/model_matrix.RData')$assocs %>%
+    group_by(pathway) %>%
+    top_n(100, -p.value) %>%
+    ungroup() %>%
+    unstack(gene ~ pathway) %>%
+    ar$split(along=2, drop=TRUE)
+
+# do: Venn diagram for the overlap for all pathways
+pathways = b$list$transpose(list('Gene Ontology'=go, Reactome=reactome, 'Perturbation-response'=speed))
+pathways = pathways[sort(names(pathways))]
+
+pdf("/dev/null")
+
+#par(mfrow=c(3,4))
+plots = lapply(seq_along(pathways), function(i) grid.arrange(gTree(children=plt$vennDiagramFromList(pathways[[i]])), top=textGrob(names(pathways)[i], gp=gpar(fontsize=30,font=8))))
+
+pdf("geneset_overlap.pdf", width=19, height=15)
+on.exit(dev.off)
+
+tg = grobTree(textGrob("Gene Ontology", y=0.65, vjust=0, gp = gpar(fontsize=25, face=3, col="blue")),
+              textGrob("Reactome", y=0.5, vjust=0, gp = gpar(fontsize=25, face=2, col="red")),
+              textGrob("Perturbation-response", y=0.35, vjust=0, gp = gpar(fontsize=25, face=3, col="green")),
+              cl="titlegrob")
+
+plots = c(plots, list(tg))
+do.call(grid.arrange, c(plots, list(ncol=4)))
+
