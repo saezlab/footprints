d311dfe8ccdb63af149957be13fdbef7ca23b521
mschubert <mschu.dev@gmail.com>
2016-05-31 22:54:46 +0100

    update plot for new-ish df format

--- analyses/drug_assocs/plot.r
+++ analyses/drug_assocs/plot.r
@@ -25,31 +25,16 @@ plot_matrix = function(assocs.pan) {
         plt$matrix(estimate ~ score + Ys)
 }
 
-plot_tissue = function(data) {
+plot_tissue = function(assocs.tissue, name) {
     # volcano plot for tissue subsets
-    tissue_plot = function(assocs.tissue, name) assocs.tissue %>%
-        mutate(label = paste(subset, Yf, score, sep=":")) %>%
+    assocs.tissue %>%
+        filter(subset == name) %>%
+        mutate(label = paste(tissue, drug, score, sep=":")) %>%
         plt$color$p_effect(pvalue="adj.p",
                            effect="estimate",
                            dir=-1,
                            thresh=0.1) %>%
         plt$volcano(p=0.1) + ggtitle(name)
-
-    for (x in seq_along(data))
-        print(tissue_plot(data[[x]], names(data)[x]))
-
-    for (tissue in unique(data$assocs.tissue_noexp$subset)) {
-        message(tissue) # if one fails easier to debug
-        p = data$assocs.tissue_noexp %>%
-            filter(subset == tissue) %>%
-            mutate(label = paste(Yf, score, sep=":")) %>%
-            plt$color$p_effect(pvalue="adj.p",
-                               effect="estimate",
-                               dir=-1,
-                               thresh=0.2) %>%
-            plt$volcano(p=0.2) + ggtitle(tissue)
-        print(p)
-    }
 }
 
 if (is.null(module_name())) {
@@ -63,9 +48,10 @@ if (is.null(module_name())) {
     pdf(OUTFILE, paper="a4r", width=11, height=8)
 
     print(plot_pancan(data$assocs.pan))
-    data$assocs.pan = NULL
 
-    plot_tissue(data)
+    print(plot_tissue(filter(data, subset=="clinical"), "clinical"))
+    print(plot_tissue(filter(data, subset=="noexp"), "no experimental"))
+    print(plot_tissue(filter(data, subset=="sensi"), "sensitive"))
 
     dev.off()
 }
