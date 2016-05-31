46c8279cd24025144b0699b2686a2327b3a99f64
mschubert <mschu.dev@gmail.com>
2016-05-31 11:57:48 +0100

    simplify drug assocs

--- analyses/drug_assocs/gdsc_assocs.r
+++ analyses/drug_assocs/gdsc_assocs.r
@@ -2,78 +2,54 @@ library(dplyr)
 b = import('base')
 io = import('io')
 ar = import('array')
+st = import('stats')
 hpc = import('hpc')
 gdsc = import('data/gdsc')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../scores/gdsc/pathways_mapped/speed_linear.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "speed_linear.RData"
+INFILE = commandArgs(TRUE)[1] %or% "../../scores/gdsc/pathways_mapped/speed_matrix.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "speed_matrix.RData"
 
-# load required data
+#' Load required data
 data = list(
     scores = io$load(INFILE),
-    Ys = gdsc$drug_response('IC50s'),
+    response = gdsc$drug_response('IC50s'),
     clinical = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2),
     noexp = gdsc$drug_response('IC50s', min_tissue_measured=0, median_top=10, stage=1),
     sensi = gdsc$drug_response('IC50s', min_tissue_measured=5, median_top=10),
     clin_sens = gdsc$drug_response('IC50s', min_tissue_measured=0, stage=2, median_top=10),
-    tissues = gdsc$tissues(minN=15),
-    MSI = gdsc$MASTER_LIST['MMR'] == "MSI-H"
+    tissues = gdsc$tissues(),
+    MSI = (gdsc$MASTER_LIST['MMR'] == "MSI-H")[,1]
 ) %>% ar$intersect_list(along=1)
-
-scores = data$scores
-tissues = data$tissues
-Ys = data$Ys
-Ysubs = data[c('clinical', 'noexp', 'sensi', 'clin_sens')]
 
 #' Tissues as covariate
 #'
-#' @param scores    A matrix of [cell lines x set or pathway scores]
-#' @param Ys        A matrix drug response [cell lines x drugs]
-#' @param scoresub  A character string of which column of `scores` to subset
-#' @param tissues   A character vector of tissues, same cell lines as `scores`
+#' @param data      A list with: tissue, MSI, scores
 #' @return          A data.frame with the association results
-pan_assocs = function(scores, Ys, scoresub, tissues) {
-    ar = import('array')
-    st = import('stats')
-
-    score = scores[, scoresub, drop=FALSE]
-    re = st$lm(Ys ~ tissues + MSI + score, data=data)
-}
+assocs.pan = st$lm(response ~ tissues + MSI + scores, data=data, min_pts=50,
+                   hpc_args = list(job_size = 5000)) %>%
+    filter(term == "scores") %>%
+    mutate(subset = tissue) %>%
+    select(-term, subset) %>%
+    mutate(adj.p = p.adjust(p.value, method="fdr"))
 
 #' Tissues as subsets
 #'
-#' @param scores    A matrix of [cell lines x set or pathway scores]
-#' @param Ysubs     A list of different subsets for drug response
-#' @param scoresub  A character string of which column of `scores` to subset
-#' @param ysub      A character string of which `Ysubs` to subset
-#' @param tissues   A character vector of tissues, same cell lines as `scores`
+#' @param resp_sub  A character string of which `response` to subset
+#' @param data      A list with: tissue, MSI, scores
 #' @return          A data.frame with the association results
-tissue_assocs = function(scores, Ysubs, scoresub, ysub, tissues) {
-    ar = import('array')
+tissue_assocs = function(resp_sub, data) {
     st = import('stats')
-
-    data = list(Yf = Ysubs[[ysub]], score = scores[,scoresub,drop=FALSE]) %>%
-        ar$intersect_list(along=1)
-
-    re = st$lm(Yf ~ MSI + score, subsets=tissues, data=data) %>%
-        mutate(Ysub = ysub)
+    data$response = data[[resp_sub]]
+    re = st$lm(response ~ MSI + scores, subsets=data$tissues, data=data, min_pts=10,
+               hpc_args = list(job_size = 5000)) %>%
+        mutate(tissue = subset, subset=resp_sub)
 }
 
-assocs.pan = hpc$Q(pan_assocs, scoresub=colnames(scores),
-                   const = list(scores=scores, Ys=Ys, tissues=tissues),
-                   job_size = 100) %>%
+assocs.tissue = sapply(c('clinical', 'noexp', 'sensi'), tissue_assocs, data=data, simplify=FALSE) %>%
     bind_rows() %>%
-    filter(term == "score") %>%
+    filter(term == "scores") %>%
     select(-term) %>%
-    mutate(adj.p = p.adjust(p.value, method="fdr"))
-
-assocs.tissue = hpc$Q(tissue_assocs, scoresub=colnames(scores), ysub=names(Ysubs),
-                      const = list(scores=scores, Ysubs=Ysubs, tissues=tissues),
-                      expand_grid=TRUE, job_size=100) %>%
-    bind_rows() %>%
-    filter(term == "score") %>%
-    select(-term) %>%
-    group_by(subset, Ysub) %>%
+    group_by(tissue, subset) %>%
     mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
     ungroup()
 
