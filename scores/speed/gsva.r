b00607e0715f79fcde2d49e3d85889c0dec59a16
mschubert <mschu.dev@gmail.com>
2016-05-31 22:22:34 +0100

    add speed gsva

---
+++ scores/speed/gsva.r
@@ -0,0 +1,51 @@
+b = import('base')
+io = import('io')
+ar = import('array')
+gdsc = import('data/gdsc')
+#gsea = import('../../util/gsea')
+hpc = import('hpc')
+
+INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/mapped/go.RData"
+EXPR = commandArgs(TRUE)[2] %or% "../../data/expr.RData"
+OUTFILE = commandArgs(TRUE)[3] %or% "pathways_mapped/gsea_go.RData"
+
+# only filter when we didn't select manually
+if (grepl("mapped", OUTFILE)) {
+    MIN_GENES = 1
+    MAX_GENES = Inf
+    job_size = 1
+} else {
+    MIN_GENES = 5
+    MAX_GENES = 500
+    job_size = 20
+}
+
+# load required data
+speed = io$load(EXPR)
+index = speed$records
+expr = speed$expr
+genesets = io$load(INFILE)
+
+#' Function to calculate GSVA score for a single signature
+#'
+#' @param set     A character describing the gene set (element in sigs)
+#' @param expr    The gene expression matrix [genes x samples]
+#' @param sigs    The list of signatures
+#' @return        Result for GSVA(expr[,sample], sigs[set])
+gsva = function(index, expr, sigs, ...) {
+	mean_func = function(x) mean(x[index$perturbed]) - mean(x[index$control])
+#	gsea$filter_genesets(rownames(expr), MIN_GENES, MAX_GENES)
+    re = GSVA::gsva(expr=expr, gset.idx.list=sigs, parallel.sz=1, ...)$es.obs
+    rowMeans(re[,index$perturbed,drop=FALSE]) - rowMeans(re[,index$control,drop=FALSE])
+}
+
+scores = hpc$Q(gsva, index=index, expr=expr, const = list(sigs=genesets),
+        memory = 1024, n_jobs = 20) %>%
+    setNames(names(index)) %>%
+    ar$stack(along=1)
+
+filter_index = function(x) x[! names(x) %in% c('control', 'perturbed', 'exclusion')]
+index = lapply(index, filter_index) %>%
+    bind_rows()
+
+save(scores, file=OUTFILE)
--- scores/speed/set_enrichment.r
+++ scores/speed/set_enrichment.r
@@ -8,26 +8,21 @@ INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/go.RData"
 EXPR = commandArgs(TRUE)[2] %or% "../../data/expr.RData"
 OUTFILE = commandArgs(TRUE)[3] %or% "go.RData"
 
-# load gene lists for pathways
-genelist = io$load(INFILE)
-
-# get index, expr data for test set
+# load required data
 speed = io$load(EXPR)
 index = speed$records
 expr = speed$expr
+genelist = io$load(INFILE)
 
 doGSEA = function(index, expr, sigs) {
     library(dplyr)
     ar = import('array')
     gsea = import('../../util/gsea')
+    mean_func = function(x) mean(x[index$perturbed]) - mean(x[index$control])
 
     gsea$runGSEA(expr=expr, sigs=sigs, normalize=TRUE) %>%
-        ar$map(along=1, function(x) mean(x[index$perturbed]) - mean(x[index$control]))
+        ar$map(along=1, mean_func)
 }
-
-# perform GSEA
-#scores = mapply(gsea$runGSEA, expr=expr,
-#    MoreArgs=list(sigs=genelist, normalize=TRUE))
 
 scores = hpc$Q(doGSEA, index=index, expr=expr, const = list(sigs=genelist),
         memory = 1024, n_jobs = 50) %>%
