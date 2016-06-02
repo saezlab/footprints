bb554eadaa87292704bb4498f35b8a627e0dadb5
mschubert <mschu.dev@gmail.com>
2016-06-02 17:22:32 +0100

    do tcga gsva per tissue (won't work otherwise)

--- scores/tcga/gsva.r
+++ scores/tcga/gsva.r
@@ -6,40 +6,37 @@ gsea = import('../../util/gsea')
 hpc = import('hpc')
 
 INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/mapped/go.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "pathways_mapped/gsea_go.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "pathways_mapped/gsva_go.RData"
 
 # only filter when we didn't select manually
 if (grepl("mapped", OUTFILE)) {
     MIN_GENES = 1
     MAX_GENES = Inf
-    job_size = 1
 } else {
     MIN_GENES = 5
     MAX_GENES = 500
-    job_size = 20
 }
 
 # load gene list and expression
-expr = lapply(tcga$tissues(), tcga$rna_seq) %>%
-    ar$stack(along=2)
-genesets = io$load(INFILE) %>%
-    gsea$filter_genesets(rownames(expr), MIN_GENES, MAX_GENES)
+tissues = import('../../config')$tcga$tissues
+expr = b$lnapply(tissues, tcga$rna_seq)
+genesets = io$load(INFILE)
 
 #' Function to calculate GSVA score for a single signature
 #'
 #' @param set     A character describing the gene set (element in sigs)
+#' @param set     A character describing the tissue (element in expr)
 #' @param expr    The gene expression matrix [genes x samples]
 #' @param sigs    The list of signatures
 #' @return        Result for GSVA(expr[,sample], sigs[set])
-gsva = function(set, expr, sigs, ...) {
-    #FIXME: this is taking too long (expr matrix too big))
-    GSVA::gsva(expr=expr, gset.idx.list=sigs[set], parallel.sz=0, ...)$es.obs
+gsva = function(expr, sigs, ...) {
+    GSVA::gsva(expr, gset.idx.list=sigs, parallel.sz=1, ...)$es.obs
 }
 
 # perform GSEA for each sample and signature
-result = hpc$Q(gsva, set = names(genesets),
-               const = list(expr=expr, sigs=genesets),
-               memory = 20480, job_size = job_size) %>%
-    ar$stack(along=1) %>% t()
+result = hpc$Q(gsva, expr=expr,
+               const = list(sigs=genesets, min.sz=MIN_GENES, max.sz=MAX_GENES),
+               memory = 10240, job_size = 1) %>%
+    ar$stack(along=2) %>% t()
 
 save(result, file=OUTFILE)
