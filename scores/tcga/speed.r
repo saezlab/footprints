8be81a5b254a64d484b0200b034f1ebd65ccc3ff
mschubert <mschu.dev@gmail.com>
2016-12-26 17:50:44 +0000

    fix scores

--- scores/tcga/speed.r
+++ scores/tcga/speed.r
@@ -6,30 +6,16 @@ tcga = import('data/tcga')
 
 INFILE = commandArgs(TRUE)[1] %or% "../../model/model_linear.RData"
 OUTFILE = commandArgs(TRUE)[2] %or% "speed_linear.RData"
+TISSUES = import('../../config')$tcga$tissues
 
-# load vectors
+# load z vectors and TCGA expression
 vecs = io$load(INFILE)$model
-
-# possible questions here:
-#  using all tumor data, is pathway activity associated with survival outcome?
-#    - filter for last known alive? [where is this field?]
-#    - subset treatment naive?
-#    - can it predict relapse?
-#    - does a treatment activate pathways?
-# -- all in covariate and subset tissue data
+expr = tcga$rna_seq(TISSUES)
 
 # calculate scores from expr and speed vectors
-tt = tcga$tissues()
-tissue2scores = function(t) {
-    cur_vecs = vecs
-    expr = tcga$rna_seq(t)
-    ar$intersect(cur_vecs, expr, along=1)
-    scores = t(expr) %*% cur_vecs %>%
-        ar$map(along=1, scale)
-}
-scores = do.call(rbind, lapply(tt, tissue2scores))
-scores = scores[!duplicated(rownames(scores)),]
+ar$intersect(vecs, expr, along=1)
+scores = t(expr) %*% vecs %>%
+    ar$map(along=1, scale)
 
-# do lapply instead of for as soon as works, then ar$stack + save
-
+# save scores
 save(scores, file=OUTFILE)
