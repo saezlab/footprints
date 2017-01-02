976ec8cfa1bcc17d57dded456f3d5da39ff8e13b
mschubert <mschu.dev@gmail.com>
2017-01-02 13:49:39 +0000

    fix ccle scores

--- scores/ccle/speed.r
+++ scores/ccle/speed.r
@@ -1,22 +1,14 @@
-# point of this file:
-# - use the different models model to calculate scores in
-#   - input data
-#   - ccle data
-#   - tcga data (later)
 b = import('base')
 io = import('io')
 ar = import('array')
 ccle = import('data/ccle')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../model/model_linear.RData"
-OUTFILE = commandArgs(TRUE)[2] %or% "speed_linear.RData"
+INFILE = commandArgs(TRUE)[1] %or% "../../model/model_matrix.RData"
+OUTFILE = commandArgs(TRUE)[2] %or% "speed_matrix.RData"
 
-# load zscores
+# load model and expression data
 zfit = io$load(INFILE)$model
-
-# load sanger data
 expr = t(ccle$basal_expression())
-#ar$intersect(expr, tissues, along=1)
 
 # calculate scores on panel
 expr = expr[,intersect(rownames(zfit),colnames(expr))]
