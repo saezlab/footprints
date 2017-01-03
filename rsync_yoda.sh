ebf692b37d8a2aac8c5bc837f6608a6db4764b7d
mschubert <mschu.dev@gmail.com>
2017-01-03 14:55:04 +0100

    new rsync

--- rsync_yoda.sh
+++ rsync_yoda.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 
 rsync -auvr --include='*.pdf' --include '[zd]scores.RData' --exclude 'data/*' \
     --include '*.RData' --include='*/' --exclude='*' \
-    yoda:/research/performance/saezrodriguez/mike/speed2/* . 
+    yoda:/hps/nobackup/saezrodriguez/mike/speed2/* .
 
 #rsync -avur figure/* yoda:/nfs/research2/saezrodriguez/mike/speed2/figure/
