f6e69c2c2f9ec2e1066f2deed6a5359e8ad4524d
mschubert <mschu.dev@gmail.com>
2017-01-11 10:58:17 +0000

    update lincs index

--- util/lincs_perturbation_qc/index.r
+++ util/lincs_perturbation_qc/index.r
@@ -4,6 +4,9 @@ io = import('io')
 df = import('data_frame')
 lincs = import('data/lincs')
 
+INFILES = commandArgs(TRUE)[-1] %or% list.files(".", "\\.txt")
+OUTFILE = commandArgs(TRUE)[1] %or% "index.RData"
+
 # index should have
 #  - control experiments (DMSO treatment)
 #  - activity-mod experiments (trt_cp, trt_lig)
@@ -12,12 +15,13 @@ lincs = import('data/lincs')
 #  - same number of experiments for all (more for control)
 
 # read .txt files
-pathways = list.files(".", "\\.txt") %>% lapply(function(fname) {
-    re = io$read_table(fname)
-    colnames(re) = c("id", "pert_desc", "pert_type", "sign")
-    re$pathway = sub("\\.txt", "", fname)
-    re
-}) %>%
+pathways = lapply(INFILES, function(fname) {
+        message(fname)
+        re = io$read_table(fname)
+        colnames(re) = c("id", "pert_desc", "pert_type", "sign")
+        re$pathway = sub("\\.txt", "", fname)
+        re
+    }) %>%
     bind_rows() %>%
     bind_rows(data.frame(pert_desc="DMSO", pathway="control", sign="0")) %>%
     filter(sign %in% c("+","-","0")) %>%
@@ -37,7 +41,8 @@ index = lincs$get_index() %>%
     filter(pert_time_unit == "h") %>%
     df$subset(pathways, add_cols=TRUE) %>%
     mutate(pert_type = type_lookup[pert_type]) %>%
-    filter(!is.na(pert_type)) %>%
+    filter(!is.na(pert_type), pert_time >= 4, pert_time <= 24)
+
 #    group_by(pathway, sign, pert_type) %>%
 #    do(sample_n(., 100, replace=TRUE)) %>% #FIXME: no replace, but let s<n be?
 #    ungroup()
