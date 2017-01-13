8535a69f3ab7c82e4c2813101d700ffa08240ee6
mschubert <mschu.dev@gmail.com>
2017-01-13 10:43:43 +0000

    load data on worker for gsva as well

--- scores/lincs/gsva.r
+++ scores/lincs/gsva.r
@@ -9,7 +9,7 @@ GENESETS = commandArgs(TRUE)[1] %or% "../../util/genesets/mapped/go.RData"
 INDEX = commandArgs(TRUE)[2] %or% "../../util/lincs/index.RData"
 OUTFILE = commandArgs(TRUE)[3] %or% "gsva_go.RData"
 
-row2scores = function(i) {
+row2scores = function(i, index, exps, sets) {
     row = index[i,]
     sign = row$sign
     ptb = df$subset(exps, row)$distil_id
@@ -22,6 +22,9 @@ row2scores = function(i) {
     if (length(ctl) > 100)
         ctl = sample(ctl, 100, replace=FALSE)
 
+    expr = lincs$get_z(cid=c(ctl,ptb), rid=lincs$projected, map_genes="hgnc_symbol")
+    gsva = GSVA::gsva(expr=expr, gset.idx.list=sets, parallel.sz=1)$es.obs
+
     gsva_ctl = gsva[ctl,,drop=FALSE]
     gsva_ptb = gsva[ptb,,drop=FALSE]
     colMeans(gsva_ptb) - colMeans(gsva_ctl)
@@ -30,20 +33,15 @@ row2scores = function(i) {
 # load model vectors and experiment index
 sets = io$load(GENESETS)
 exps = io$load(INDEX)
-expr = lincs$get_z(exps$distil_id, rid=lincs$projected, map_genes="hgnc_symbol")
-
-# calculate scores for all samples
-gsva_fun = function(set, sets, expr)
-    GSVA::gsva(expr=expr, gset.idx.list=sets[set], parallel.sz=1)$es.obs
-gsva = clustermq::Q(gsva_fun, set=names(sets), const=list(sets=sets, expr=expr),
-                    memory=10240, job_size=1) %>% ar$stack(along=2) %>% t()
 
 index = exps %>%
     select(pathway, cell_id, pert_id, pert_dose, pert_time, sign) %>%
     filter(pathway != "control") %>%
     distinct()
 
-scores = pbapply::pblapply(seq_len(nrow(index)), row2scores) %>%
+scores = clustermq::Q(row2scores, i=seq_len(nrow(index)),
+                      const=list(index=index, exps=exps, sets=sets),
+                      memory=10240, n_jobs=50) %>%
     setNames(seq_len(nrow(index))) %>%
     ar$stack(along=1) %>%
     ar$map(along=1, scale)
