67210a882380e50aa8f8800e299750460e39ad12
mschubert <mschu.dev@gmail.com>
2017-01-31 22:25:30 +0000

    fix dataset size figure

--- analyses/signature_overview/dataset_size.r
+++ analyses/signature_overview/dataset_size.r
@@ -6,7 +6,7 @@ records = io$load('../../data/expr.RData')$records
 index = io$load('../../data/zscores.RData')$index # the ones we actually use (pass qc, etc)
 gatza2009 = io$read_table('../../util/genesets/ng.3073-S2_info.txt', header=TRUE)
 
-speed1 = list(
+gsva_speed2016 = list(
     Pathways = 11,
     Datasets = 69,
     Experiments = 215,
@@ -27,12 +27,12 @@ speed_matrix = lapply(list(
     Arrays = unique(unlist(lapply(records, function(x) c(x$control, x$perturbed))))
 ), length)
 
-gatza = list(
+gsva_gatza = list(
     Pathways = 18, #nrow(gatza2009), # too many duplicates/non-signaling -> use 2009 exps
     Datasets = 1, #length(unique(unlist(stringr::str_match_all(gatza2009$References, "[0-9]+")))),
     Experiments = 18, # nrow(gatza2009),
     Arrays = 287 # by summing up numbers in supp fig. 4
 )
 
-df = cbind(measure=names(speed1), speed1, speed2, speed_matrix, gatza)
+df = cbind(measure=names(gsva_speed2016), gsva_speed2016, speed_matrix, gsva_gatza)
 io$write_table(df, file="dataset_size.txt", sep="\t")
