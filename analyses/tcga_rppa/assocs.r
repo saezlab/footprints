77dda0cfc9afac6fc2a876ebfec9c402fc23ef8b
mschubert <mschu.dev@gmail.com>
2017-02-02 17:20:36 +0000

    do assocs for pancan and tissue

--- analyses/tcga_rppa/assocs.r
+++ analyses/tcga_rppa/assocs.r
@@ -6,17 +6,23 @@ tcga = import('data/tcga')
 INFILE = "../../scores/tcga/pathways_mapped/speed_matrix.RData"
 OUTFILE = "assocs.RData"
 
-# load expression, RPPA for all cancers where both available
+# load required pathway scores and rppa data
 rppa = t(tcga$rppa())
 scores = io$load(INFILE)
 tcga$intersect(rppa, scores, along=1)
 
 tissues = tcga$barcode2study(rownames(scores))
 
-result = st$lm(scores ~ rppa, subsets=tissues, hpc_args=list(n_jobs=10)) %>%
+# calculate associations across and within tissues
+pan = st$lm(scores ~ tissues + rppa) %>%
     filter(term == "rppa") %>%
+    select(-term)
+
+tissue = st$lm(scores ~ rppa, subsets=tissues, hpc_args=list(n_jobs=10)) %>%
+    filter(term == "rppa") %>%
+    select(-term) %>%
     group_by(subset) %>%
     mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
     ungroup()
 
-save(result, file=OUTFILE)
+save(pan, tissue, file=OUTFILE)
