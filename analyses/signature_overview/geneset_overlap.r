019ef793b16da56c320577ebe51f5644ee83cfc7
mschubert <mschu.dev@gmail.com>
2017-02-05 13:40:37 +0000

    add working overlap calc

--- analyses/signature_overview/geneset_overlap.r
+++ analyses/signature_overview/geneset_overlap.r
@@ -5,7 +5,7 @@ ar = import('array')
 plt = import('plot')
 config = import('../../config')
 
-get_genesets = function() {
+get_genesets = function() { #FIXME: JAK.STAT
     # import all sets from methods listed in config
     # missing: SPIA (KEGG gene sets, include?) and PARADIGM (no sets per se)
     sets = c("go", "reactome", "speed2016", "gatza")
@@ -31,10 +31,36 @@ get_genesets = function() {
         select(method=method, pathway=ind, gene=values)
 }
 
-geneset_overlap_matrix = function(sets) {
-    # compute their overlap
+set2overlap = function(sets, fun=function(x,y) nrow(intersect(x,y))) {
+    uniquify_colnames = function(df) {
+        colnames(df) = make.names(colnames(df), unique=TRUE)
+        df
+    }
 
-    # return a ggplot upper diagonal matrix
+    sets %>%
+        group_by(method) %>%
+        tidyr::nest() %>%
+        tidyr::crossing(., ., by="method") %>%
+        uniquify_colnames() %>%
+        transmute(method1 = method,
+                  method2 = method.1,
+                  overlap = purrr::map2_int(data, data.1, fun))
+}
+
+geneset_overlap_matrix = function(sets, formula=value ~ Var1 + Var2) {
+    overlaps = sets %>%
+        group_by(pathway) %>%
+        do(set = set2overlap(.)) %>%
+        ungroup() %>%
+        tidyr::unnest()
+
+    plt$matrix(overlaps, formula, palette="Blues") +
+        geom_text(aes(label=all.vars(formula)[1])) +
+        coord_fixed() +
+        theme(legend.position = "none") +
+        facet_wrap(~pathway) + 
+        xlab("") +
+        ylab("")
 }
 
 if (is.null(module_name())) {
