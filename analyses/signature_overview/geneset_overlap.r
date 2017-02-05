c954a38413e81278337a1d2788c58b40bcfb28ed
mschubert <mschu.dev@gmail.com>
2017-02-05 12:02:36 +0000

    get_genesets function as df working

--- analyses/signature_overview/geneset_overlap.r
+++ analyses/signature_overview/geneset_overlap.r
@@ -1,15 +1,37 @@
 library(dplyr)
-library(grid)
-library(gridExtra)
 b = import('base')
 io = import('io')
 ar = import('array')
 plt = import('plot')
-config = import('../../config/methods.yaml')
+config = import('../../config')
 
-set_overlap_matrix = function() {
+get_genesets = function() {
     # import all sets from methods listed in config
+    # missing: SPIA (KEGG gene sets, include?) and PARADIGM (no sets per se)
+    sets = c("go", "reactome", "speed2016", "gatza")
 
+    set_path = module_file('../../util/genesets/mapped')
+    sets = b$lnapply(sets, function(s)
+        io$load(file.path(set_path, paste0(s, ".RData"))))
+
+    speed = io$load(module_file('../../model/model_matrix.RData'))$assocs %>%
+        group_by(pathway) %>%
+        top_n(100, -p.value) %>%
+        ungroup() %>%
+        unstack(gene ~ pathway) %>%
+        ar$split(along=2, drop=TRUE)
+
+    names(sets) = paste0("gsva_", names(sets))
+    sets = c(speed_matrix=list(speed), sets)
+    names(sets) = config$id2short(names(sets))
+
+    sets = lapply(seq_along(sets), function(i)
+              data.frame(method = names(sets)[i], stack(sets[[i]]))) %>%
+        bind_rows() %>%
+        select(method=method, pathway=ind, gene=values)
+}
+
+geneset_overlap_matrix = function(sets) {
     # compute their overlap
 
     # return a ggplot upper diagonal matrix
@@ -19,5 +41,6 @@ if (is.null(module_name())) {
     pdf("geneset_overlap.pdf", width=19, height=15)
     on.exit(dev.off)
 
-    set_overlap_matrix()
+    sets = get_genesets()
+    set_overlap_matrix(sets)
 }
