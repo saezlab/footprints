cc88c1c18a86b0c54bb2b2504face633e8d413ea
mschubert <mschu.dev@gmail.com>
2017-02-07 13:48:28 +0000

    whitespace

--- util/genesets/filter.r
+++ util/genesets/filter.r
@@ -5,24 +5,24 @@
 #' @param min       The minimum number of genes in a list to keep the list
 #' @param max       The maximum number of genes in a list to keep the list
 filter = function(genesets, valid, min=5, max=500, warn=TRUE) {
-	if (any(is.na(valid)))
-		warning("NA found in valid set")
-	if (any(valid == ""))
-		warning("empty identifier found in valid set")
+    if (any(is.na(valid)))
+        warning("NA found in valid set")
+    if (any(valid == ""))
+        warning("empty identifier found in valid set")
 
-	genesets = lapply(genesets, function(x) intersect(x, valid))
+    genesets = lapply(genesets, function(x) intersect(x, valid))
 
-	num_overlap = sapply(genesets, length)
-	discard = num_overlap < min | num_overlap > max
+    num_overlap = sapply(genesets, length)
+    discard = num_overlap < min | num_overlap > max
 
-	if (any(discard) && warn) {
-		warning("Discarding ", sum(discard), " (of ", length(discard), ") sets: ",
+    if (any(discard) && warn) {
+        warning("Discarding ", sum(discard), " (of ", length(discard), ") sets: ",
                 paste(names(genesets)[discard], collapse=", "))
-		genesets = genesets[!discard]
-	}
+        genesets = genesets[!discard]
+    }
 
     if (length(genesets) == 0)
         stop("No gene sets left after filtering")
 
-	genesets
+    genesets
 }
