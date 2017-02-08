2fd0b1a4cae8579dbaeef512bc1e888439dae92f
mschubert <mschu.dev@gmail.com>
2017-02-08 17:30:08 +0000

    add hypergeom test

---
+++ analyses/signature_overview/speed_go_hypergeom.r
@@ -0,0 +1,22 @@
+library(dplyr)
+b = import('base')
+io = import('io')
+ar = import('array')
+st = import('stats')
+plt = import('plot')
+gsea = import('../../util/gsea')
+genesets = import('../../util/genesets')
+
+OUTFILE = commandArgs(TRUE)[1] %or% "speed_go_hypergeom.RData"
+
+# we're loading this twice here just to get ground truth
+# could be more efficient ..
+assocs = io$load('../../model/model_matrix.RData')$assocs
+zfit = ar$construct(zscore ~ gene + pathway, data=assocs)
+
+speed = genesets$get("go", mapped=TRUE, to_data_frame=FALSE)$PROGENy
+go = genesets$get("go", mapped=FALSE, to_data_frame=FALSE)[["Gene Ontology"]]
+go = genesets$filter(go, valid=rownames(zfit), min=5, max=200)
+
+result = st$hypergeometric_test(rownames(zfit), go, speed)
+save(result, file=OUTFILE)
