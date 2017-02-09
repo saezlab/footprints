0042c52279779363b053100e8d23f01d8af1c7f7
mschubert <mschu.dev@gmail.com>
2017-02-09 17:20:22 +0000

    add working plot

--- analyses/signature_overview/speed_go_plot.r
+++ analyses/signature_overview/speed_go_plot.r
@@ -19,6 +19,29 @@ plot_piano = function(index) {
 #'
 #' @param mat  A matrix of p-values
 plot_hypergeom = function(mat) {
+    df = reshape2::melt(mat) %>%
+        select(set=Var1, pathway=Var2, p.value=value) %>%
+        mutate(adj.p = p.adjust(p.value, method="fdr"),
+               pathway = factor(pathway, levels=gtools::mixedsort(
+                    as.character(unique(pathway))))) %>%
+        arrange(p.value) %>%
+        group_by(pathway) %>%
+        top_n(10, -p.value) %>%
+        ungroup() %>%
+        mutate(set = reorder(set, -adj.p),
+               adj.p = -log10(adj.p))
+
+    ggplot(df, aes(x=set, y=adj.p)) +
+        geom_bar(stat='identity', fill="lightblue") +
+        geom_hline(yintercept=-log10(0.1), linetype="dotted") +
+        coord_flip() +
+        geom_text(aes(y=0, label=paste(" ", set)), hjust=0) +
+        facet_wrap(~ pathway, scales="free") +
+        theme_minimal() +
+        theme(axis.ticks.y = element_blank(),
+              axis.text.y = element_blank()) +
+        xlab("Gene Ontology category") +
+        ylab("- log10 (FDR)")
 }
 
 if (is.null(module_name())) {
