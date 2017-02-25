a842ab250e91f98cea3ee50be3b3a2c2f6c7728f
mschubert <mschu.dev@gmail.com>
2017-02-25 21:48:25 +0000

    expr processing

--- analyses/signature_validation/expr.r
+++ analyses/signature_validation/expr.r
@@ -4,14 +4,15 @@ ma = import('process/microarray')
 
 process_record = function(rec) {
     re = rec$expr
-    expr = ArrayExpress::ArrayExpress(re$accession)[[re$platform]] %>%
-        ma$qc() %>%
+    message(re$accession, " ", re$platform)
+    expr = ArrayExpress::ArrayExpress(re$accession, drop=FALSE)[[re$platform]] %>%
+#        ma$qc() %>%
         ma$normalize() %>%
         ma$annotate(summarize="hgnc_symbol")
 }
 
 index = io$read_yaml("validation.yaml")
-expr = lapply(process_record)
+expr = lapply(index, process_record)
 names(expr) = lapply(index, function(x) x$expr$accession)
 
 save(expr, file="expr.RData")
