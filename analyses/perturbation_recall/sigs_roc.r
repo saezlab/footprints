6bef3d91463ab99a06814cbff393493037d4c76d
mschubert <mschu.dev@gmail.com>
2017-03-06 16:47:05 +0000

    plot zscores+gsva

--- analyses/perturbation_recall/sigs_roc.r
+++ analyses/perturbation_recall/sigs_roc.r
@@ -2,28 +2,32 @@ io = import('io')
 st = import('stats')
 roc = import('./roc')
 
-scoredf = io$load('sigs_scores.RData') %>%
+zdf = io$load('sigs_zscores.RData') %>%
     roc$scores2df() %>%
-    mutate(inferred = sub("\\..*$", "", signature))
+    mutate(inferred = sub("\\..*$", "", signature),
+           method = "zscore")
 
-# consensus
+gsvadf = io$load('sigs_gsva.RData') %>%
+    roc$scores2df() %>%
+    mutate(inferred = sub("\\..*$", "", signature),
+           method = "gsva")
+
+scoredf = dplyr::bind_rows(zdf, gsvadf)
 
 # per signature, how well do we infer perturbed pathway?
 roc = scoredf %>%
     na.omit() %>%
     mutate(matched = perturbed == inferred) %>%
-    group_by(inferred, signature) %>%
+    group_by(method, inferred, signature) %>%
     do(st$roc(., "score", "matched")) %>%
     ungroup()
 
 width=1
 random_line = data.frame(x=c(0,1), y=c(0,1), method=roc$signature[1])
-ggplot(roc, aes(x=FPR, y=TPR)) +
+ggplot(roc, aes(x=FPR, y=TPR, color=method)) +
     geom_line(aes(x=x, y=y), data=random_line, color="grey", linetype="dashed", size=width) +
     geom_step(size=width, stat="summary", fun.y=median) +
-    geom_ribbon(stat="summary", fun.data="mean_sdl", alpha=0.2) +
-#    geom_step(size=width, stat="summary", fun.y=max, linetype="dotted") +
-#    geom_step(size=width, stat="summary", fun.y=min, linetype="dotted") +
+#    geom_ribbon(stat="summary", fun.data="mean_sdl", alpha=0.2) +
     coord_fixed() +
     facet_wrap(~inferred) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
