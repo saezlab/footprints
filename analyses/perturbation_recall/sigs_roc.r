61baa522a7b6a1c0fd47270e11f51772bc4ffc34
mschubert <mschu.dev@gmail.com>
2017-03-06 11:51:30 +0000

    whitespace

--- analyses/perturbation_recall/sigs_roc.r
+++ analyses/perturbation_recall/sigs_roc.r
@@ -6,6 +6,8 @@ scoredf = io$load('sigs_scores.RData') %>%
     roc$scores2df() %>%
     mutate(inferred = sub("\\..*$", "", signature))
 
+# consensus
+
 # per signature, how well do we infer perturbed pathway?
 roc = scoredf %>%
     na.omit() %>%
@@ -17,10 +19,10 @@ roc = scoredf %>%
 width=1
 random_line = data.frame(x=c(0,1), y=c(0,1), method=roc$signature[1])
 ggplot(roc, aes(x=FPR, y=TPR)) +
-	geom_line(aes(x=x, y=y), data=random_line, color="grey", linetype="dashed", size=width) +
-	geom_step(size=width, stat="summary", fun.y=median) +
-	geom_step(size=width, stat="summary", fun.y=max, linetype="dotted") +
-	geom_step(size=width, stat="summary", fun.y=min, linetype="dotted") +
-	coord_fixed() +
-	facet_wrap(~inferred) +
-	theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    geom_line(aes(x=x, y=y), data=random_line, color="grey", linetype="dashed", size=width) +
+    geom_step(size=width, stat="summary", fun.y=median) +
+    geom_step(size=width, stat="summary", fun.y=max, linetype="dotted") +
+    geom_step(size=width, stat="summary", fun.y=min, linetype="dotted") +
+    coord_fixed() +
+    facet_wrap(~inferred) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
