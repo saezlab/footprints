15d1ad2e51b3e390993b2d70b17316ef6cf903aa
mschubert <mschu.dev@gmail.com>
2017-03-06 12:09:27 +0000

    ribbon for median+sd

--- analyses/perturbation_recall/sigs_roc.r
+++ analyses/perturbation_recall/sigs_roc.r
@@ -21,8 +21,9 @@ random_line = data.frame(x=c(0,1), y=c(0,1), method=roc$signature[1])
 ggplot(roc, aes(x=FPR, y=TPR)) +
     geom_line(aes(x=x, y=y), data=random_line, color="grey", linetype="dashed", size=width) +
     geom_step(size=width, stat="summary", fun.y=median) +
-    geom_step(size=width, stat="summary", fun.y=max, linetype="dotted") +
-    geom_step(size=width, stat="summary", fun.y=min, linetype="dotted") +
+    geom_ribbon(stat="summary", fun.data="mean_sdl", alpha=0.2) +
+#    geom_step(size=width, stat="summary", fun.y=max, linetype="dotted") +
+#    geom_step(size=width, stat="summary", fun.y=min, linetype="dotted") +
     coord_fixed() +
     facet_wrap(~inferred) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
