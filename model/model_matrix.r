36aa5d8d5b8a2d0966bd7b96947fadddfe888f47
mschubert <mschu.dev@gmail.com>
2017-03-14 22:26:38 +0000

    do without wrapped stats/lm

--- model/model_matrix.r
+++ model/model_matrix.r
@@ -11,31 +11,45 @@ io = import('io')
 zscore2model = function(zdata, hpc_args=NULL) {
     b = import('base')
     ar = import('array')
+    df = import('data_frame')
     st = import('stats')
 
     index = zdata$index
-    zscores = t(zdata$zscores)
+    zscores = t(zdata$zscores) * index$sign
 
     # create indicator matrix which pathway is activated/inhibited
-    pathway = index$sign * (ar$mask(index$pathway) + 0)
+#    pathway = index$sign * (ar$mask(index$pathway) + 0)
+    pathway = ar$mask(index$pathway) + 0
     pathway[,"MAPK"] = pathway[,"MAPK"] + pathway[,"EGFR"]
     pathway[,"NFkB"] = pathway[,"NFkB"] + pathway[,"TNFa"]
     # add EGFR>PI3K link here?
 
     # if using intercept
+    #FIXME: st$lm doesn't treat 1 + pathway correctly
     pathway = cbind('(Intercept)'=1, pathway)
 
-    # use intercept here?
-    mod = st$lm(zscores ~ 0 + pathway, data=index, min_pts=30, atomic="pathway",
-                hpc_args=hpc_args) %>%
-        transmute(gene = zscores,
+    my_lm = function(z, pathway) {
+        mylm = import('ebits/stats/export_indexed/lm')$lm
+        mylm(z ~ 0 + pathway, data=list(z=z, pathway=pathway))
+    }
+
+    # this avoids st$lm bugs; but: how to iter 2 vars? (tidyr::crossing?)
+    mod = clustermq::Q(my_lm,
+            z = zscores,
+            const = list(pathway=pathway),
+            n_jobs=20) %>%
+        setNames(colnames(zscores)) %>%
+        b$omit$null() %>%
+        df$add_name_col(col="gene", bind=TRUE) %>%
+        transmute(gene = gene,
                   pathway = sub("^pathway", "", term),
                   zscore = estimate,
                   p.value = p.value) %>%
+        filter(pathway != "(Intercept)") %>%
         mutate(adj.p = p.adjust(p.value, method="fdr"))
 
-    zfit = ar$construct(zscore ~ gene + pathway, data=mod)
-    pval = ar$construct(p.value ~ gene + pathway, data=mod)
+    zfit = ar$construct(zscore ~ gene + pathway, data=mod, fill=0)
+    pval = ar$construct(p.value ~ gene + pathway, data=mod, fill=1)
 
     # filter zfit to only include top 100 genes per pathway
     model = zfit
