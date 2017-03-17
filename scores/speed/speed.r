356c11d8552bcc69389cc0f454e9f607a3abf56a
mschubert <mschu.dev@gmail.com>
2017-03-17 15:36:15 +0000

    structure jobs to be faster

--- scores/speed/speed.r
+++ scores/speed/speed.r
@@ -44,9 +44,10 @@ if (is.null(module_name())) {
     zdata2model = import_(sub("\\.r$", "", MODEL))$zscore2model
     expr = io$load(EXPR)
 
-    scores = clustermq::Q(expr2scores, id=index$id, job_size=1,
+    scores = clustermq::Q(expr2scores, id=index$id, job_size=10, memory=20480,
               const = list(expr=expr, zdata=zdata,
-                           zdata2model=zdata2model)) %>%
+                           zdata2model=zdata2model,
+                           hpc_args = list(n_jobs=10))) %>%
         setNames(zdata$index$id) %>%
         ar$stack(along=1) %>%
         ar$map(along=1, scale) %>% # scale each pathway across all experiments
