84735bee4a573df8543c374e66e385685ee3a81b
mschubert <mschu.dev@gmail.com>
2017-03-18 14:58:09 +0000

    handle input/output file args better

--- analyses/signature_similarity/tsne_df.r
+++ analyses/signature_similarity/tsne_df.r
@@ -6,8 +6,8 @@ df = import('data_frame')
 fnames = c("top100_z.RData", "top100_de.RData", "fdr_de.RData", "speed_matrix.RData")
 file2name = setNames(c("Top 100 z-score", "Top 100 DE", "10% FDR", "speed_matrix"), fnames)
 
-INFILES = unname(commandArgs(TRUE)[1:4] %or% fnames)
-OUTFILE = commandArgs(TRUE)[4] %or% "tsne_df.RData"
+OUTFILE = commandArgs(TRUE)[1] %or% "tsne_df.RData"
+INFILES = unname(commandArgs(TRUE)[-1] %or% fnames)
 
 file2tsne = function(fname) {
     mat = t(io$load(fname))
