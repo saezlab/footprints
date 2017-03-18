43a51a100aed57ac4be6e2499dd1c0c1b0c90f48
mschubert <mschu.dev@gmail.com>
2017-03-18 14:51:04 +0000

    do all

--- analyses/signature_similarity/tsne_df.r
+++ analyses/signature_similarity/tsne_df.r
@@ -3,10 +3,10 @@ b = import('base')
 io = import('io')
 df = import('data_frame')
 
-fnames = c("top100_z.RData", "top100_de.RData", "fdr_de.RData")
-file2name = setNames(c("Top 100 z-score", "Top 100 DE", "10% FDR"), fnames)
+fnames = c("top100_z.RData", "top100_de.RData", "fdr_de.RData", "speed_matrix.RData")
+file2name = setNames(c("Top 100 z-score", "Top 100 DE", "10% FDR", "speed_matrix"), fnames)
 
-INFILES = unname(commandArgs(TRUE)[1:3] %or% fnames)
+INFILES = unname(commandArgs(TRUE)[1:4] %or% fnames)
 OUTFILE = commandArgs(TRUE)[4] %or% "tsne_df.RData"
 
 file2tsne = function(fname) {
