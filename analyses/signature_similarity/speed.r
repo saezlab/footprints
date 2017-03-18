3b2d7957ed78fd78ef6ccd73a95de79c893513e6
mschubert <mschu.dev@gmail.com>
2017-03-18 14:38:04 +0000

    remove imputed nonsense

--- analyses/signature_similarity/speed.r
+++ analyses/signature_similarity/speed.r
@@ -1,10 +1,9 @@
 library(dplyr)
-library(bhtsneR)
 b = import('base')
 io = import('io')
 ar = import('array')
 
-INFILE = commandArgs(TRUE)[1] %or% '../../scores/speed/speed_fullmat.RData'
+INFILE = commandArgs(TRUE)[1] %or% '../../scores/speed/speed_matrix.RData'
 OUTFILE = commandArgs(TRUE)[2] %or% "/dev/null"
 
 data = io$load(INFILE)
