b4d4bbf52515fe87a243db598751d5409ef217d9
mschubert <mschu.dev@gmail.com>
2017-03-18 14:43:37 +0000

    fix transform

--- analyses/signature_similarity/speed.r
+++ analyses/signature_similarity/speed.r
@@ -7,16 +7,6 @@ INFILE = commandArgs(TRUE)[1] %or% '../../scores/speed/speed_matrix.RData'
 OUTFILE = commandArgs(TRUE)[2] %or% "/dev/null"
 
 data = io$load(INFILE)
-index = data$index
-
 scores = data$scores
-scores[is.na(scores)] = 0 #FIXME:
 
-stopifnot(rownames(scores) == index$id)
-dim2 = tsne(scores)
-
-index = index %>%
-    mutate(x = dim2[,1],
-           y = dim2[,2])
-
-save(index, file=OUTFILE)
+save(scores, file=OUTFILE)
