f27c36927924f9da66812cb64bcf132bfb442e34
mschubert <mschu.dev@gmail.com>
2017-03-30 17:44:32 +0100

    more parallel

--- scores/speed/speed.r
+++ scores/speed/speed.r
@@ -44,10 +44,11 @@ if (is.null(module_name())) {
     zdata2model = import_(sub("\\.r$", "", MODEL))$zscore2model
     expr = io$load(EXPR)
 
-    scores = clustermq::Q(expr2scores, id=index$id, job_size=10, memory=20480,
-              const = list(expr=expr, zdata=zdata,
-                           zdata2model=zdata2model,
-                           hpc_args = list(n_jobs=10, memory=2048))) %>%
+    scores = clustermq::Q(expr2scores, id=index$id, job_size=10, memory=10240,
+              const = list(expr = expr,
+                           zdata = zdata,
+                           zdata2model = zdata2model,
+                           hpc_args = list(n_jobs=100, memory=2048))) %>%
         setNames(zdata$index$id) %>%
         ar$stack(along=1) %>%
         ar$map(along=1, scale) #%>% # scale each pathway across all experiments
