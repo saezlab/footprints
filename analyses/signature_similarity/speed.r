bc49d13682885645c71cb978675e1660d8f64c21
mschubert <mschu.dev@gmail.com>
2017-04-18 20:07:48 +0200

    show rel scores

--- analyses/signature_similarity/speed.r
+++ analyses/signature_similarity/speed.r
@@ -7,6 +7,10 @@ INFILE = commandArgs(TRUE)[1] %or% '../../scores/speed/speed_matrix.RData'
 OUTFILE = commandArgs(TRUE)[2] %or% "/dev/null"
 
 data = io$load(INFILE)
-scores = t(data$scores * data$index$sign)
+
+# we already scale=1 in scores
+# this is to show relative pathway activations
+scores = t(data$scores * data$index$sign) %>%
+    ar$map(along=2, scale)
 
 save(scores, file=OUTFILE)
--- analyses/signature_similarity/tsne_df.r
+++ analyses/signature_similarity/tsne_df.r
@@ -4,7 +4,7 @@ io = import('io')
 df = import('data_frame')
 
 fnames = c("top100_z.RData", "top100_de.RData", "fdr_de.RData", "speed_matrix.RData")
-file2name = setNames(c("Top 100 z-score", "Top 100 DE", "10% FDR", "speed_matrix"), fnames)
+file2name = setNames(c("Top 100 z-score", "Top 100 DE", "10% FDR", "PROGENy"), fnames)
 
 OUTFILE = commandArgs(TRUE)[1] %or% "tsne_df.RData"
 INFILES = unname(commandArgs(TRUE)[-1] %or% fnames)
