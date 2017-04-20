d70bb7606e9c6594c87c3b7c6275e8fd1e486cd1
mschubert <mschu.dev@gmail.com>
2017-04-20 23:02:16 +0100

    do not normalize

--- scores/speed/set_enrichment.r
+++ scores/speed/set_enrichment.r
@@ -3,7 +3,7 @@ b = import('base')
 io = import('io')
 ar = import('array')
 
-INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/go.RData"
+INFILE = commandArgs(TRUE)[1] %or% "../../util/genesets/mapped/go.RData"
 EXPR = commandArgs(TRUE)[2] %or% "../../data/expr.RData"
 OUTFILE = commandArgs(TRUE)[3] %or% "go.RData"
 
@@ -19,15 +19,15 @@ doGSEA = function(index, expr, sigs) {
     gsea = import('../../util/gsea')
     mean_func = function(x) mean(x[index$perturbed]) - mean(x[index$control])
 
-    gsea$runGSEA(expr=expr, sigs=sigs, normalize=TRUE) %>%
+    gsea$runGSEA(expr=expr, sigs=sigs) %>% #, normalize=TRUE) %>%
         ar$map(along=1, mean_func)
 }
 
 scores = clustermq::Q(doGSEA, index=index, expr=expr, const = list(sigs=genelist),
         memory = 1024, n_jobs = 50) %>%
     setNames(names(index)) %>%
-    ar$stack(along=1) %>%
-    ar$map(along=1, scale) #%>% # normalize different magnitude of pathways
+    ar$stack(along=1) #%>%
+#    ar$map(along=1, scale) #%>% # normalize different magnitude of pathways
 #    ar$map(along=2, scale) # normalize total activation per experiment
 
 filter_index = function(x) x[! names(x) %in% c('control', 'perturbed', 'exclusion')]
