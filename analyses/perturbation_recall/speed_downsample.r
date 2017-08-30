f4bf5c29e55e278cb9a3097b53ecd3a76b298e4b
mschubert <mschu.dev@gmail.com>
2017-08-30 17:42:10 +0100

    working dev

--- analyses/perturbation_recall/speed_downsample.r
+++ analyses/perturbation_recall/speed_downsample.r
@@ -1,41 +1,78 @@
-library(dplyr)
-library(magrittr)
-b = import('base')
-import('./speed', attach=TRUE)
+downsampled_auc = function(n_sigs, expr, zdata, zdata2model) {
+    library(dplyr)
+    library(magrittr)
+    index = zdata$index
+
+    keep = zdata$index %>%
+        group_by(pathway) %>%
+        mutate(ni = sample(1:n(), size=n())) %>%
+        filter(ni <= n_sigs) %$%
+        id
+
+    # build model
+    zdata$zscores = zdata$zscores[1:10,keep] #TODO:
+    zdata$index = zdata$index[match(keep, zdata$index$id),]
+    stopifnot(zdata$index$id == colnames(zdata$zscores))
+    model = zdata2model(zdata, hpc_args=list(n_jobs=0))$model
+
+    # score experiments
+    score_one = function(vecs, expr, index) {
+        narray::intersect(vecs, expr, along=1)
+        mat = t(expr) %*% vecs
+        ctl = mat[index$control,,drop=FALSE]
+        ptb = mat[index$perturbed,,drop=FALSE]
+        colMeans(ptb) - colMeans(ctl)
+    }
+    scores = mapply(score_one, expr=expr$expr, index=expr$records,
+                    MoreArgs = list(vecs=model)) %>%
+        t() %>%
+        narray::map(along=1, scale)
+
+    # calculate ROC AUC
+    st = import('stats')
+    roc = import('./roc_util')
+    auc = list(scores=scores, index=index[match(rownames(scores), index$id),]) %>%
+        roc$scores2df() %>%
+        na.omit() %>%
+        mutate(method = "speed_matrix",
+               inferred = signature,
+               matched = perturbed == inferred) %>%
+        group_by(inferred, signature) %>%
+        do(st$roc(., "score", "matched")) %>%
+        ungroup() %>%
+        roc$roc2auc() %>%
+        transmute(pathway=inferred, auc=PROGENy)
+}
 
 if (is.null(module_name())) {
+    library(dplyr)
+    library(magrittr)
+    b = import('base')
+    io = import('io')
+
     MODEL = commandArgs(TRUE)[1] %or% "../../model/model_matrix.r"
     EXPR = commandArgs(TRUE)[2] %or% "../../data/expr.RData"
     ZSCORES = commandArgs(TRUE)[3] %or% "../../data/zscores.RData"
-    OUTFILE = commandArgs(TRUE)[4] %or% "speed_matrix_50.RData"
-    FRAC = commandArgs(TRUE)[5] %or% 50
+    OUTFILE = commandArgs(TRUE)[4] %or% "speed_downsample_auc.RData"
 
     # load zscores, model building function, and expression for each experiment
     zdata = io$load(ZSCORES)
-    index = zdata$index
     zdata2model = import_(sub("\\.r$", "", MODEL))$zscore2model
     expr = io$load(EXPR)
 
-    keep = index %>%
+    max_sigs = zdata$index %>%
         group_by(pathway) %>%
-        sample_frac(as.numeric(FRAC)/100) %$%
-        id
+        summarize(n=n())
 
-    zdata$zscores = zdata$zscores[,keep]
-    zdata$index = zdata$index[match(keep, zdata$index$id),]
-    stopifnot(zdata$index$id == colnames(zdata$zscores))
+    n_sigs = 3:max(max_sigs$n)
+    aucs = clustermq::Q(downsampled_auc, n_sigs=n_sigs, n_jobs=10,#job_size=1,
+                        const = list(expr=expr, zdata=zdata, zdata2model=zdata2model))
 
-    scores = clustermq::Q(expr2scores, id=index$id, job_size=1, memory=10240,
-              const = list(expr = expr,
-                           zdata = zdata,
-                           zdata2model = zdata2model)) %>% #,
-#                           hpc_args = list(n_jobs=20, memory=2048))) %>%
-        setNames(zdata$index$id) %>%
-        ar$stack(along=1) %>%
-        ar$map(along=1, scale) #%>% # scale each pathway across all experiments
-#        ar$map(along=2, scale) # scale each experiment across all pathways
+    aucs = data_frame(n_sigs=n_sigs, auc=aucs) %>%
+        tidyr::unnest() %>%
+        dplyr::left_join(max_sigs, by="pathway") %>%
+        filter(n_sigs <= n) %>%
+        select(-n)
 
-    stopifnot(zdata$index$id == rownames(scores))
-
-    save(scores, index, file=OUTFILE)
+    save(aucs, file=OUTFILE)
 }
