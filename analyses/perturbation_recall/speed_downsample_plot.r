5caac90803a6621cca07e94a32a8355932995a22
mschubert <mschu.dev@gmail.com>
2017-09-05 14:21:49 +0200

    add downsample auc with roc

--- analyses/perturbation_recall/speed_downsample_plot.r
+++ analyses/perturbation_recall/speed_downsample_plot.r
@@ -2,6 +2,10 @@ library(ggplot2)
 b = import('base')
 io = import('io')
 
+aucdf = function(fname="speed_downsample_auc.RData") {
+    io$load(module_file(fname))
+}
+
 do_plot = function(aucdf) {
     ggplot(aucdf, aes(x=n_sigs, y=auc)) +
         geom_point() +
@@ -13,7 +17,7 @@ if (is.null(module_name())) {
     INFILE = commandArgs(TRUE)[1] %or% "speed_downsample_auc.RData"
     OUTFILE = commandArgs(TRUE)[2] %or% "speed_downsample_plot.pdf"
 
-    aucdf = io$load(INFILE)
+    aucdf = aucdf(INFILE)
     pdf(OUTFILE)
     print(do_plot(aucdf))
     dev.off()
